1f50d81f6efd35e553404702015c1a3d2c7f391a
Benjamin Kott <info@bk2k.info>
2014-08-19 22:32:10 +0200

    Cleanup less.php integration - fixes #32

--- ext_autoload.php
+++ ext_autoload.php
@@ -2,5 +2,5 @@
 
 $extensionPath = PATH_site.'typo3conf/ext/bootstrap_package/';
 return array(
-    'Less_Autoloader'   => $extensionPath . 'Contrib/less.php/lib/Less/Autoloader.php',
+    'Less_Autoloader'   => $extensionPath . 'Contrib/less.php/Autoloader.php',
 );
