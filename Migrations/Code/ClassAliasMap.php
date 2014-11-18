880b198bd981a58e266c3efbfd2c3639767ca4ea
Benjamin Kott <benjamin.kott@outlook.com>
2014-11-18 22:09:08 +0100

    Add migrations for realurl to be compatible with CMS 7.0

---
+++ Migrations/Code/ClassAliasMap.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+    't3lib_div' => 'BK2K\\BootstrapPackage\\Migration\\T3libDivMigration',
+    't3lib_pageSelect' => 'TYPO3\\CMS\\Frontend\\Page\\PageRepository',
+    't3lib_iconWorks' => 'TYPO3\\CMS\\Backend\\Utility\\IconUtility',
+    't3lib_utility_Math' => 'TYPO3\\CMS\\Core\\Utility\\MathUtility',
+    't3lib_cacheHash' => 'TYPO3\\CMS\\Frontend\\Page\\CacheHashCalculator',
+    't3lib_TStemplate' => 'TYPO3\\CMS\\Core\\TypoScript\\TemplateService',
+    't3lib_extMgm' => 'TYPO3\\CMS\\Core\\Utility\\ExtensionManagementUtility',
+);
---
+++ Migrations/Code/LegacyClassesForIde.php
@@ -0,0 +1,10 @@
+<?php
+die('Access denied');
+
+class t3lib_div extends \BK2K\BootstrapPackage\Migration\T3libDivMigration {}
+class t3lib_pageSelect extends \TYPO3\CMS\Frontend\Page\PageRepository {}
+class t3lib_iconWorks extends \TYPO3\CMS\Backend\Utility\IconUtility {}
+class t3lib_utility_Math extends \TYPO3\CMS\Core\Utility\MathUtility {}
+class t3lib_cacheHash extends \TYPO3\CMS\Frontend\Page\CacheHashCalculator {}
+class t3lib_TStemplate extends \TYPO3\CMS\Core\TypoScript\TemplateService {}
+class t3lib_extMgm extends \TYPO3\CMS\Core\Utility\ExtensionManagementUtility {}
