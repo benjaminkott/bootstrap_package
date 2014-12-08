f0508a9d9b63826d279d95bfa224c5f5087d0d50
Benjamin Kott <benjamin.kott@wfp2.com>
2014-12-08 18:02:38 +0100

    [BUGFIX] Flashmessage queue throws error while installing - fixes #116

--- Classes/Service/InstallService.php
+++ Classes/Service/InstallService.php
@@ -28,38 +28,56 @@ namespace BK2K\BootstrapPackage\Service;
  ***************************************************************/
 
 use TYPO3\CMS\Core\Messaging\FlashMessage;
-use TYPO3\CMS\Core\Messaging\FlashMessageQueue;
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Utility\VersionNumberUtility;
 
 /**
  * @author Benjamin Kott <info@bk2k.info>
  */
 class InstallService {
 
+	/**
+	 * @var string
+	 */
 	protected $extKey = 'bootstrap_package';
+
+	/**
+	 * @var string
+	 */
+	protected $messageQueueByIdentifier = '';
+
+	/**
+	 * Initializes the install service
+	 */
+	public function __construct(){
+		if(VersionNumberUtility::convertVersionNumberToInteger(TYPO3_version) >= 7000000){
+			$this->messageQueueByIdentifier = 'extbase.flashmessages.tx_extensionmanager_tools_extensionmanagerextensionmanager';
+		}else{
+			$this->messageQueueByIdentifier = 'core.template.flashMessages';
+		}
+	}
 
 	/**
 	 * @param string $extension
 	 */
 	public function generateApacheHtaccess($extension = NULL) {
-
 		if ($extension == $this->extKey) {
 			if (substr($_SERVER['SERVER_SOFTWARE'], 0, 6) === 'Apache') {
 				$this->createDefaultHtaccessFile();
 			} else {
 
 				/**
-				 * Add Flashmessage that the system it not running on an apache webserver and the url rewritings must be handled manually
+				 * Add Flashmessage that the system is not running on an apache webserver and the url rewritings must be handled manually
 				 */
-				$message = GeneralUtility::makeInstance('TYPO3\\CMS\\Core\\Messaging\\FlashMessage',
+				$flashMessage = GeneralUtility::makeInstance('TYPO3\\CMS\\Core\\Messaging\\FlashMessage',
 					'The Bootstrap Package uses RealUrl to generate SEO friendly URLs by default, please take care of the URLs rewriting settings for your environment yourself.<br>'
 					. 'You can also deactivate RealUrl by changing your TypoScript setup to "<strong>config.tx_realurl_enable = 0</strong>".',
 					'TYPO3 is not running on an Apache-Webserver',
 					FlashMessage::WARNING,
 					TRUE
 				);
-				FlashMessageQueue::addMessage($message);
+				$this->addFlashMessage($flashMessage);
 				return;
 			}
 		}
@@ -71,39 +89,50 @@ class InstallService {
 	 * @return void
 	 */
 	public function createDefaultHtaccessFile() {
-
 		$htaccessFile = GeneralUtility::getFileAbsFileName(".htaccess");
-
 		if (file_exists($htaccessFile)) {
 
 			/**
 			 * Add Flashmessage that there is already an .htaccess file and we are not going to override this.
 			 */
-			$message = GeneralUtility::makeInstance('TYPO3\\CMS\\Core\\Messaging\\FlashMessage',
+			$flashMessage = GeneralUtility::makeInstance('TYPO3\\CMS\\Core\\Messaging\\FlashMessage',
 				'There is already an Apache .htaccess file in the root directory, please make sure that the url rewritings are set properly.<br>'
 				. 'An example configuration is located at: <strong>typo3conf/ext/bootstrap_package/Configuration/Apache/.htaccess</strong>',
 				'Apache .htaccess file already exists',
 				FlashMessage::NOTICE,
 				TRUE
 			);
-			FlashMessageQueue::addMessage($message);
+			$this->addFlashMessage($flashMessage);
 			return;
 		}
-
 		$htaccessContent = GeneralUtility::getUrl(ExtensionManagementUtility::extPath($this->extKey) . '/Configuration/Apache/.htaccess');
 		GeneralUtility::writeFile($htaccessFile, $htaccessContent, TRUE);
 
 		/**
 		 * Add Flashmessage that the example htaccess file was placed in the root directory
 		 */
-		$message = GeneralUtility::makeInstance('TYPO3\\CMS\\Core\\Messaging\\FlashMessage',
+		$flashMessage = GeneralUtility::makeInstance('TYPO3\\CMS\\Core\\Messaging\\FlashMessage',
 			'For RealURL and optimization purposes an example .htaccess file was placed in your root directory. <br>'
 			. ' Please check if the RewriteBase correctly set for your environment. ',
 			'Apache example .htaccess was placed in the root directory.',
 			FlashMessage::OK,
 			TRUE
 		);
-		FlashMessageQueue::addMessage($message);
+		$this->addFlashMessage($flashMessage);
 	}
 
+	/**
+	 * Adds a Flash Message to the Flash Message Queue
+	 *
+	 * @param FlashMessage $flashMessage
+	 */
+	public function addFlashMessage(FlashMessage $flashMessage){
+		if($flashMessage){
+			/** @var $flashMessageService \TYPO3\CMS\Core\Messaging\FlashMessageService */
+			$flashMessageService = GeneralUtility::makeInstance('TYPO3\\CMS\\Core\\Messaging\\FlashMessageService');
+			/** @var $flashMessageQueue \TYPO3\CMS\Core\Messaging\FlashMessageQueue */
+			$flashMessageQueue = $flashMessageService->getMessageQueueByIdentifier($this->messageQueueByIdentifier);
+			$flashMessageQueue->enqueue($flashMessage);
+		}
+	}
 }
