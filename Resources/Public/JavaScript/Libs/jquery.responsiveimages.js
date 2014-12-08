f8ad4af72c987c2fa8db404bb0db549f646bdcfb
Benjamin Kott <benjamin.kott@outlook.com>
2014-12-08 23:21:46 +0100

    [TASK] Minify responsiveimages.js and and cleanup

--- Resources/Public/JavaScript/Libs/jquery.responsiveimages.js
+++ Resources/Public/JavaScript/Libs/jquery.responsiveimages.js
@@ -19,13 +19,11 @@
 					1140: 'bigger'
 				},
 				container:window,
-                                skip_invisible: true
+				skip_invisible: true
 			},
 			options = $.extend({}, defaults, options),
 			$window = $(window),
-			threshold = options.threshold || 0,
 			breakpoints = options.breakpoints,
-			hdpi = window.devicePixelRatio > 1,
 			attrib = "data-src",
 			images = this,
 			originimages = this,
@@ -48,37 +46,38 @@
 					attrib = "data-" + datakey;
 				}
 			});
-			if (old_attrib != attrib) {
+			if (old_attrib !== attrib) {
 				images = originimages;
 			}
 			responsiveimages();
 		}
 		function inviewport($el, options){
-        		var $c = $(options.container)    
-        		,   ew = $el.width()
-        		,   eh = $el.height()
-        		,   el = $el.offset().left    
-        		,   et = $el.offset().top    
-        		,   cw = $c.width()   
-        		,   ch    
-        		,   cl  
-        		,   ct;
-          		if (options.container === undefined || options.container === window){
-            			ch = window.innerHeight ? window.innerHeight : $window.height();
-            			cl = $window.scrollLeft();
-            			ct = $window.scrollTop();
-          		} else {
-            			ch = $c.height();
-            			cl = $c.offset().left;
-            			ct = $c.offset().top;
-          		}
-          		return cl + cw > el - options.threshold && cl < el + ew + options.threshold && ct + ch > et - options.threshold && ct < et + eh + options.threshold   
-        	}
+			var $c = $(options.container),
+				ew = $el.width(),
+				eh = $el.height(),
+				el = $el.offset().left,
+				et = $el.offset().top,
+				cw = $c.width(),
+				ch,
+				cl,
+				ct;
+			if (options.container === undefined || options.container === window){
+				ch = window.innerHeight ? window.innerHeight : $window.height();
+				cl = $window.scrollLeft();
+				ct = $window.scrollTop();
+			} else {
+				ch = $c.height();
+				cl = $c.offset().left;
+				ct = $c.offset().top;
+			}
+			return cl + cw > el - options.threshold && cl < el + ew + options.threshold && ct + ch > et - options.threshold && ct < et + eh + options.threshold;
+		}
+
 		function responsiveimages() {
 			var inview = images.filter(function () {
 				var $element = $(this);
 				if (options.skip_invisible && $element.is(":hidden")) return;
-				return inviewport($element, options);  
+				return inviewport($element, options);
 			});
 			loaded = inview.trigger("responsiveimages");
 			images = images.not(loaded);
@@ -86,11 +85,11 @@
 
 		$(options.container).scroll(checkviewport);
 		$window.resize(checkviewport);
- 		$.fn.responsiveimages.update = checkviewport;
+		$.fn.responsiveimages.update = checkviewport;
 		checkviewport();
 
 		return this;
 
-	}
+	};
 
 })(window.jQuery);
