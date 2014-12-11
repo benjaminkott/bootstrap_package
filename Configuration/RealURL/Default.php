18f1ca82db520abe4328302f0ec933aab312752d
Aimeos <aimeos@aimeos.org>
2014-12-11 14:28:07 +0100

    Don't overwrite existing configuration completely

--- Configuration/RealURL/Default.php
+++ Configuration/RealURL/Default.php
@@ -1,50 +1,38 @@
 <?php
 
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl'] = array(
-	'_DEFAULT' => array(
-		'init' => array(
-			'enableCHashCache' => TRUE,
-			'appendMissingSlash' => 'ifNotFile',
-			'enableUrlDecodeCache' => TRUE,
-			'enableUrlEncodeCache' => TRUE,
-			'emptyUrlReturnValue' => \TYPO3\CMS\Core\Utility\GeneralUtility::getIndpEnv('TYPO3_SITE_URL'),
-		),
-		'pagePath' => array(
-			'type' => 'user',
-			'userFunc' => 'EXT:realurl/class.tx_realurl_advanced.php:&tx_realurl_advanced->main',
-			'spaceCharacter' => '-',
-			'languageGetVar' => 'L',
-		),
-		'redirects' => array(),
-		'preVars' => array(
-			'0' => array(
-				'GETvar' => 'no_cache',
-				'valueMap' => array(
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['_DEFAULT']['init']['enableCHashCache'] = TRUE;
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['_DEFAULT']['init']['appendMissingSlash'] = 'ifNotFile';
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['_DEFAULT']['init']['enableUrlDecodeCache'] = TRUE;
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['_DEFAULT']['init']['enableUrlEncodeCache'] = TRUE;
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['_DEFAULT']['init']['emptyUrlReturnValue'] = \TYPO3\CMS\Core\Utility\GeneralUtility::getIndpEnv('TYPO3_SITE_URL');
+
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['_DEFAULT']['pagePath']['type'] = 'user';
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['_DEFAULT']['pagePath']['userFunc'] = 'EXT:realurl/class.tx_realurl_advanced.php:&tx_realurl_advanced->main';
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['_DEFAULT']['pagePath']['spaceCharacter'] = '-';
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['_DEFAULT']['pagePath']['languageGetVar'] = 'L';
+
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['_DEFAULT']['preVars'] = array(
+	'0' => array(
+			'GETvar' => 'no_cache',
+			'valueMap' => array(
 					'nc' => '1',
-				),
-				'noMatch' => 'bypass'
 			),
-			'1' => array(
-				'GETvar' => 'L',
-				'valueMap' => array(
+			'noMatch' => 'bypass'
+	),
+	'1' => array(
+			'GETvar' => 'L',
+			'valueMap' => array(
 					'da' => '1',
 					'de' => '2',
-				),
-				'noMatch' => 'bypass',
 			),
-		),
-		'fixedPostVars' => array(),
-		'postVarSets' => array(
-			'_DEFAULT' => array(
-				'page' => array(
-					0 => array(
-						'GETvar' => 'page',
-					),
-				),
-			),
-		),
-		'fileName' => array(
-			'defaultToHTMLsuffixOnPrev' => true,
-		),
-	)
+			'noMatch' => 'bypass',
+	),
 );
+
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['_DEFAULT']['postVarSets']['_DEFAULT']['page'] = array(
+	0 => array(
+			'GETvar' => 'page',
+	),
+);
+
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['_DEFAULT']['fileName']['defaultToHTMLsuffixOnPrev'] = TRUE;
