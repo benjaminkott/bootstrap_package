c056ffd936a09033db2c5f38a9524f177428d232
Benjamin Kott <benjamin.kott@wfp2.com>
2014-12-12 09:13:50 +0100

    [TASK] Rise dependency of TYPO3 and css_styled_content version to 7.99.99
    Because Thomas Löffler said its possible ;)

--- ext_emconf.php
+++ ext_emconf.php
@@ -28,8 +28,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => NULL,
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.6-7.0.99',
-			'css_styled_content' => '6.2.0-7.0.99',
+			'typo3' => '6.2.6-7.99.99',
+			'css_styled_content' => '6.2.0-7.99.99',
 		),
 		'conflicts' => array(
 			'fluidpages' => '*',
