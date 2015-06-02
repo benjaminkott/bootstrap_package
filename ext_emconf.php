848ec4efb51ff19ee48162afcbc2b03421da41c5
Benjamin Kott <benjamin.kott@wfp2.com>
2015-06-02 09:39:11 +0200

    [TASK] Cleanup extension declaration file to match documentation

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,39 +1,28 @@
 <?php
 
-/***************************************************************
+/************************************************************************
  * Extension Manager/Repository config file for ext "bootstrap_package".
- ***************************************************************/
-
+ ************************************************************************/
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'Bootstrap Package',
-	'description' => 'Bootstrap Package delivers a full configured frontend for TYPO3 CMS 6.2, based on the Bootstrap CSS Framework.',
-	'category' => 'templates',
-	'shy' => 0,
-	'version' => '6.2.11-dev',
-	'dependencies' => '',
-	'conflicts' => '',
-	'priority' => 'top',
-	'loadOrder' => '',
-	'module' => '',
-	'state' => 'stable',
-	'uploadfolder' => 0,
-	'createDirs' => '',
-	'modify_tables' => 'tt_content',
-	'clearcacheonload' => 1,
-	'lockType' => '',
-	'author' => 'Benjamin Kott',
-	'author_email' => 'info@bk2k.info',
-	'author_company' => 'private',
-	'CGLcompliance' => NULL,
-	'CGLcompliance_note' => NULL,
-	'constraints' => array(
-		'depends' => array(
-			'typo3' => '6.2.12-7.99.99',
-			'css_styled_content' => '6.2.0-7.99.99',
-		),
-		'conflicts' => array(
-			'fluidpages' => '*',
-			'dyncss' => '*',
-		),
-	),
+    'title' => 'Bootstrap Package',
+    'description' => 'Bootstrap Package delivers a full configured frontend for TYPO3 CMS, based on the Bootstrap CSS Framework.',
+    'category' => 'templates',
+    'constraints' => array(
+        'depends' => array(
+            'typo3' => '6.2.12-7.99.99',
+            'css_styled_content' => '6.2.0-7.99.99',
+        ),
+        'conflicts' => array(
+            'fluidpages' => '*',
+            'dyncss' => '*',
+        ),
+    ),
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 1,
+    'author' => 'Benjamin Kott',
+    'author_email' => 'info@bk2k.info',
+    'author_company' => 'private',
+    'version' => '6.2.11-dev',
 );
