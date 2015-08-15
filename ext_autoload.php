d359261cf8c5f70d4b22d4107212ac9ac3fe1c61
Benjamin Kott <benjamin.kott@outlook.com>
2015-08-15 12:48:03 +0200

    [TASK] Update less.php to 1.7.0.5

--- ext_autoload.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$extensionPath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('bootstrap_package');
-return array(
-	'Less_Autoloader' => $extensionPath . 'Contrib/less.php/Autoloader.php',
-);
