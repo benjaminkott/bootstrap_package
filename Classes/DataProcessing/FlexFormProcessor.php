22d60c8b442a64351f89b75f8ff421034e5b2cb9
Benjamin Kott <benjamin.kott@outlook.com>
2015-12-02 23:55:29 +0100

    [!!!][TASK] Replace FlexFormViewHelper with FlexFormProcessor

---
+++ Classes/DataProcessing/FlexFormProcessor.php
@@ -0,0 +1,98 @@
+<?php
+namespace BK2K\BootstrapPackage\DataProcessing;
+
+/*
+ *  The MIT License (MIT)
+ *
+ *  Copyright (c) 2015 Benjamin Kott, http://www.bk2k.info
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Service\FlexFormService;
+use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
+use TYPO3\CMS\Frontend\ContentObject\DataProcessorInterface;
+
+/**
+ * Minimal TypoScript configuration
+ * Process field pi_flexform and overrides the values stored in data
+ *
+ * 10 = BK2K\BootstrapPackage\DataProcessing\FlexFormProcessor
+ *
+ *
+ * Advanced TypoScript configuration
+ * Process field assigned in fieldName and stores processed data to new key
+ *
+ * 10 = BK2K\BootstrapPackage\DataProcessing\FlexFormProcessor
+ * 10 {
+ *   fieldName = pi_flexform
+ *   as = flexform
+ * }
+ */
+class FlexFormProcessor implements DataProcessorInterface
+{
+    /**
+     * @var FlexFormService
+     */
+    protected $flexFormService;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->flexFormService = GeneralUtility::makeInstance(FlexFormService::class);
+    }
+
+    /**
+     * @param ContentObjectRenderer $cObj The data of the content element or page
+     * @param array $contentObjectConfiguration The configuration of Content Object
+     * @param array $processorConfiguration The configuration of this processor
+     * @param array $processedData Key/value store of processed data (e.g. to be passed to a Fluid View)
+     * @return array the processed data as key/value store
+     */
+    public function process(ContentObjectRenderer $cObj, array $contentObjectConfiguration, array $processorConfiguration, array $processedData)
+    {
+        // The field name to process
+        $fieldName = $cObj->stdWrapValue('fieldName', $processorConfiguration);
+        if (empty($fieldName) && !$processedData['data']['pi_flexform']) {
+            return $processedData;
+        } else {
+            $fieldName = 'pi_flexform';
+        }
+
+        // Process Flexform
+        $originalValue = $processedData['data'][$fieldName];
+        if (!is_string($originalValue)) {
+            return $processedData;
+        }
+        $flexformData = $this->flexFormService->convertFlexFormContentToArray($originalValue);
+
+        // Set the target variable
+        $targetVariableName = $cObj->stdWrapValue('as', $processorConfiguration);
+        if (!empty($targetVariableName)) {
+            $processedData[$targetVariableName] = $flexformData;
+        } else {
+            $processedData['data'][$fieldName] = $flexformData;
+        }
+
+        return $processedData;
+    }
+}
