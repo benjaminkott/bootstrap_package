863c9fd972c966636fc6e5e2e6a8b81216e482aa
Benjamin Kott <benjamin.kott@outlook.com>
2015-12-03 16:48:32 +0100

    [TASK] Add TCA for content element text

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,6 +1,42 @@
 <?php
 
 /***************
+ * Add Content Element: Text
+ */
+$GLOBALS['TCA']['tt_content']['ctrl']['typeicon_classes'] += [
+    'text' => 'mimetypes-x-content-text'
+];
+array_splice(
+    $GLOBALS['TCA']['tt_content']['columns']['CType']['config']['items'],
+    2,
+    0,
+    [[
+        'LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:CType.I.1',
+        'text',
+        'content-text'
+    ]]
+);
+$GLOBALS['TCA']['tt_content']['types']['text'] = [
+    'showitem' => '
+        --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.general;general,
+        --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.header;header,
+        bodytext;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:bodytext_formlabel,
+        --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
+        --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.frames;frames,
+        --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.access,
+        --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.visibility;visibility,
+        --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.access;access,
+        --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.extended,
+        rowDescription',
+    'columnsOverrides' => [
+        'bodytext' => [
+            'defaultExtras' => 'richtext:rte_transform[mode=ts_css]'
+        ]
+    ]
+];
+
+
+/***************
  * Add Content Elements to List
  */
 $backupCTypeItems = $GLOBALS['TCA']['tt_content']['columns']['CType']['config']['items'];
