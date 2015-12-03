40ceba9ce145c8b771829458e93f4021ab81965e
Benjamin Kott <benjamin.kott@outlook.com>
2015-12-03 22:23:57 +0100

    [TASK] Add TCA and wizard for content element div

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -273,6 +273,23 @@ if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('t3editor')) {
 
 
 /***************
+ * Add Content Element: Div
+ */
+$GLOBALS['TCA']['tt_content']['types']['div'] = [
+    'showitem' => '
+        --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.general;general,
+        header;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:header.ALT.div_formlabel,
+        --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
+        --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.frames;frames,
+        --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.access,
+        --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.visibility;visibility,
+        --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.access;access,
+        --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.extended,
+        rowDescription'
+];
+
+
+/***************
  * Add Content Elements to List
  */
 $backupCTypeItems = $GLOBALS['TCA']['tt_content']['columns']['CType']['config']['items'];
