540e7df8c9b3d9eb9a17e1aa52380b677363a82a
Benjamin Kott <benjamin.kott@outlook.com>
2015-12-13 02:57:37 +0100

    [TASK] Add FluidTemplate for list

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -384,6 +384,33 @@ $GLOBALS['TCA']['tt_content']['types']['shortcut'] = array_replace_recursive(
 
 
 /***************
+ * Add Content Element: List
+ */
+if (!is_array($GLOBALS['TCA']['tt_content']['types']['list'])) {
+    $GLOBALS['TCA']['tt_content']['types']['list'] = [];
+}
+$GLOBALS['TCA']['tt_content']['types']['list'] = array_replace_recursive(
+    $GLOBALS['TCA']['tt_content']['types']['list'],
+    [
+        'showitem' => '
+            --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.general;general,
+            header;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:header.ALT.script_formlabel,
+            list_type;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:list_type_formlabel,
+            select_key;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:select_key_formlabel,
+            pages;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:pages.ALT.list_formlabel,
+            recursive,
+            --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
+            --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.frames;frames,
+            --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.access,
+            hidden;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:field.default.hidden,
+            --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.access;access,
+            --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.extended,
+            rowDescription'
+    ]
+);
+
+
+/***************
  * Add Content Elements to List
  */
 $backupCTypeItems = $GLOBALS['TCA']['tt_content']['columns']['CType']['config']['items'];
