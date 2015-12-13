9b06797e206b7a65f3f034832c9eba6d4723d97e
Benjamin Kott <benjamin.kott@outlook.com>
2015-12-13 02:43:19 +0100

    [TASK] Add typoscript setup as content rendering template

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,6 +3,11 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
+/***************
+ * Define TypoScript as content rendering template
+ */
+$GLOBALS['TYPO3_CONF_VARS']['FE']['contentRenderingTemplates'][] = 'bootstrappackage/Configuration/TypoScript/';
+
 
 /***************
  * Make the extension configuration accessible
