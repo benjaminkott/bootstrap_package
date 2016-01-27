317032be4ea2d7875bd37dbb14fbb0d1317cdeea
Benjamin Kott <benjamin.kott@wfp2.com>
2016-01-27 16:35:53 +0100

    [FEATURE] Allow photoswipe to be opened by url params

--- Resources/Public/JavaScript/Src/bootstrap.lightbox.js
+++ Resources/Public/JavaScript/Src/bootstrap.lightbox.js
@@ -1,10 +1,9 @@
 $(function () {
 
     /**
-     * Basic lightbox implementation based on Bootstrap modals
+     * Add PhotoSwipe template to dom if lightbox elements exist.
      */
     if ($('a.lightbox').length > 0) {
-
         var photoswipeTemplate = '\
             <div class="pswp" tabindex="-1" role="dialog" aria-hidden="true">\
                 <div class="pswp__bg"></div>\
@@ -41,32 +40,101 @@ $(function () {
                 </div>\
             </div>';
         $('body').append(photoswipeTemplate);
-        $('a.lightbox').on('click',function(e){
-            e.preventDefault();
-            $(this).addClass('photoswipe-trigger');
-            var photoswipeContainer = document.querySelectorAll('.pswp')[0];
-            var $trigger = $(this);
-            var index = 0;
-            var rel = $(this).attr('rel');
-            var items = [];
-            $('a.lightbox[rel=' + rel + ']').each(function(){
-                if($(this).hasClass('photoswipe-trigger')){
-                    index = items.length;
-                }
-                items.push({
-                    src: $(this).attr('href'),
-                    title: $(this).attr("title"),
-                    w: $(this).data('lightbox-width'),
-                    h: $(this).data('lightbox-height')
-                });
+    }
+
+    /**
+     * Open PhotoSwipe
+     */
+    var openPhotoSwipe = function(pid, gid) {
+        var photoswipeContainer = document.querySelectorAll('.pswp')[0];
+        var items = [];
+
+        $('a.lightbox[rel=' + gid + ']').each(function(){
+            var caption = '';
+            if($(this).data('lightbox-caption')) {
+                caption = $(this).data('lightbox-caption');
+            } else {
+                caption = $(this).next('figcaption').text();
+            }
+            items.push({
+                src: $(this).attr('href'),
+                title: $(this).attr("title"),
+                w: $(this).data('lightbox-width'),
+                h: $(this).data('lightbox-height'),
+                caption: caption,
+                pid: $(this).index('a.lightbox[rel=' + gid + ']'),
             });
-            var options = {
-                index: index
-            };
+        });
+        var options = {
+            index: pid,
+            addCaptionHTMLFn: function(item, captionEl, isFake) {
+                if(!item.title) {
+                    captionEl.children[0].innerHTML = '';
+                    return false;
+                }
+                captionEl.children[0].innerHTML = '<div class="pswp__caption__title">' + item.title + '</div>';
+                if(item.caption) {
+                    captionEl.children[0].innerHTML += '<div class="pswp__caption__subtitle">' + item.caption + '</div>';
+                }
+                return true;
+            },
+            spacing:        0.12,
+            loop:           true,
+            bgOpacity:      1,
+            closeOnScroll:  true,
+            history:        true,
+            galleryUID:     gid,
+            galleryPIDs:    true,
+            closeEl:        true,
+            captionEl:      true,
+            fullscreenEl:   true,
+            zoomEl:         true,
+            shareEl:        true,
+            counterEl:      true,
+            arrowEl:        true,
+            preloaderEl:    true,
+        };
+        if(items.length > 0) {
             var gallery = new PhotoSwipe( photoswipeContainer, PhotoSwipeUI_Default, items, options);
             gallery.init();
-            $(this).removeClass('photoswipe-trigger');
-        });
+        }
+    }
+
+    /**
+     * Get PhotoSwipe params from url hash and open gallery
+     */
+    var getPhotoSwipeParamsFromHash = function() {
+        var hash = window.location.hash.substring(1),
+        params = {};
+        if(hash.length < 5) {
+            return params;
+        }
+        var vars = hash.split('&');
+        for (var i = 0; i < vars.length; i++) {
+            if(!vars[i]) {
+                continue;
+            }
+            var pair = vars[i].split('=');
+            if(pair.length < 2) {
+                continue;
+            }
+            params[pair[0]] = pair[1];
+        }
+        return params;
     };
+    var photoSwipeHashData = getPhotoSwipeParamsFromHash();
+    if(photoSwipeHashData.pid && photoSwipeHashData.gid) {
+        openPhotoSwipe( parseInt(photoSwipeHashData.pid) , photoSwipeHashData.gid);
+    }
+
+    /**
+     * Register listener
+     */
+    $('a.lightbox').on('click',function(e){
+        e.preventDefault();
+        var gid = $(this).attr('rel');
+        var pid = $(this).index('a.lightbox[rel=' + gid + ']');
+        openPhotoSwipe(pid, gid);
+    });
 
 });
