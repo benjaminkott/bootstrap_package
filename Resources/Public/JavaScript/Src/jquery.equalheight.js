c278f609eb4708f671f9e58396a57966f3967725
Benjamin Kott <benjamin.kott@wfp2.com>
2016-03-22 12:20:52 +0100

    [RELEASE] Release of 7.0.1 for TER release with TYPO3 8.0

--- Resources/Public/JavaScript/Src/jquery.equalheight.js
+++ Resources/Public/JavaScript/Src/jquery.equalheight.js
@@ -1,5 +1,5 @@
 /* ==========================================================================================
- * Bootstrap Package: jquery.equalheight.js v7.0.0
+ * Bootstrap Package: jquery.equalheight.js v7.0.1
  * -----------
  * Measure height of columns in a row and set their height to the tallest one.
  *
