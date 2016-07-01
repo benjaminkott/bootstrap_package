ef7bcf098769e18591b21590b1ae36d30068be16
Benjamin Kott <benjamin.kott@wfp2.com>
2016-07-01 13:13:56 +0200

    [FEATURE] Add content element to display regular text in columns

---
+++ Configuration/TCA/Overrides/tt_content_element_textcolumn.php
@@ -0,0 +1,42 @@
+<?php
+
+/***************
+ * Add Content Element: Text
+ */
+$GLOBALS['TCA']['tt_content']['ctrl']['typeicon_classes']['textcolumn'] = 'content-bootstrappackage-textcolumn';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTcaSelectItem(
+    'tt_content',
+    'CType',
+    [
+        'LLL:EXT:bootstrap_package/Resources/Private/Language/Backend.xlf:content_element.textcolumn',
+        'textcolumn',
+        'content-bootstrappackage-textcolumn'
+    ],
+    'text',
+    'after'
+);
+if (!is_array($GLOBALS['TCA']['tt_content']['types']['texttextcolumncolumns'])) {
+    $GLOBALS['TCA']['tt_content']['types']['textcolumn'] = [];
+}
+$GLOBALS['TCA']['tt_content']['types']['textcolumn'] = array_replace_recursive(
+    $GLOBALS['TCA']['tt_content']['types']['textcolumn'],
+    [
+        'showitem' => '
+            --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.general;general,
+            --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.header;header,
+            bodytext,
+            --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
+            --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.frames;frames,
+            --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.access,
+            --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.visibility;visibility,
+            --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.access;access,
+            --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.extended,
+            rowDescription',
+        'columnsOverrides' => [
+            'bodytext' => [
+                'defaultExtras' => 'richtext:rte_transform[mode=ts_css]'
+            ]
+
+        ]
+    ]
+);
--- ext_tables.php
+++ ext_tables.php
@@ -67,6 +67,11 @@ $iconRegistry->registerIcon(
     ['source' => 'EXT:bootstrap_package/Resources/Public/Icons/ContentElements/tab-item.svg']
 );
 $iconRegistry->registerIcon(
+    'content-bootstrappackage-textcolumn',
+    \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+    ['source' => 'EXT:bootstrap_package/Resources/Public/Icons/ContentElements/textcolumn.svg']
+);
+$iconRegistry->registerIcon(
     'content-bootstrappackage-texticon',
     \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
     ['source' => 'EXT:bootstrap_package/Resources/Public/Icons/ContentElements/texticon.svg']
