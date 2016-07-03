19d159044d49bbd02722663d3bfb487e380538b5
Benjamin Kott <benjamin.kott@wfp2.com>
2016-07-03 12:52:11 +0200

    [TASK] Move icon selector to new tab to have enough space for large icon sets - relates #357

--- Configuration/TCA/Overrides/tt_content_element_bootstrap_texticon.php
+++ Configuration/TCA/Overrides/tt_content_element_bootstrap_texticon.php
@@ -53,9 +53,10 @@ $GLOBALS['TCA']['tt_content']['types']['bootstrap_package_texticon'] = array_rep
         'showitem' => '
             --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.general;general,
             --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.header;header,
-            --palette--;LLL:EXT:bootstrap_package/Resources/Private/Language/Backend.xlf:field.icon;bootstrap_package_icons,
             bodytext;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:bodytext_formlabel,
             rte_enabled;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:rte_enabled_formlabel,
+            --div--;LLL:EXT:bootstrap_package/Resources/Private/Language/Backend.xlf:tabs.icon,
+            --palette--;LLL:EXT:bootstrap_package/Resources/Private/Language/Backend.xlf:field.icon;bootstrap_package_icons,
             --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
             --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.frames;frames,
             --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.access,
