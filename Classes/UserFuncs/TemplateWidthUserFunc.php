65fc7e77bbfa7f0a5dd2bfd6533406da688984e9
s-leger <stephen@3dservices.ch>
2016-10-13 15:18:01 +0200

    fix indent
    first attempt to fix indent for travis

--- Classes/UserFuncs/TemplateWidthUserFunc.php
+++ Classes/UserFuncs/TemplateWidthUserFunc.php
@@ -1,5 +1,6 @@
 <?php
   namespace BK2K\BootstrapPackage\UserFuncs;
+
   /*
   *  The MIT License (MIT)
   *
@@ -23,81 +24,93 @@
   *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
   *  THE SOFTWARE.
   */
-  
+
   /**
   * @author Benjamin Kott <info@bk2k.info>
   */
-  class TemplateWidthUserFunc {
+class TemplateWidthUserFunc
+{
     /**
     * Reference to the parent (calling) cObject set from TypoScript
     */
     public $cObj;
-     
-    public function storeToRegister($content="", $conf){
-      
-      $xs = 0;
-      $sm = 0;
-      $md = 0;
-      $lg = 0;
-      $fluid = 0;
-      if (isset($conf['fluid.'])) {
-        $fluid = (bool)$this->cObj->stdWrap($conf['fluid'], $conf['fluid.']);
-      } 
-      if (isset($conf['xs.'])) {
-        $xs = $this->cObj->stdWrap($conf['xs'], $conf['xs.']);
-      } 
-      if ($xs == 0){
-        $xs = 12;
-      }
-      if (isset($conf['sm.'])) {
-        $sm = $this->cObj->stdWrap($conf['sm'], $conf['sm.']);
-      } 
-      if ($sm == 0){
-        $sm = $xs;
-      }
-      if (isset($conf['md.'])) {
-        $md = $this->cObj->stdWrap($conf['md'], $conf['md.']);
-      } 
-      if ($md == 0){
-        $md = $sm;
-      }
-      if (isset($conf['lg.'])) {
-        $lg = $this->cObj->stdWrap($conf['lg'], $conf['lg.']);
-      } 
-      if ($lg == 0){
-        $lg = $md;
-      }
-      // TODO: put this into settings / plugin config as they cant stay hardcoded
-      $gutter = 30;
-      $maxcols = 12;
-      
-      // fluid width => next container width 
-      if ($fluid){
-        $w = array (
-          'xxs' => 450, // container xs
-          'xs' => 720,  // container sm
-          'sm' => 940,  // container md
-          'md' => 1140, // container lg
-          'lg' => 1920  // container xl
-        );
-      } else {
-        // fixed (xs always fluid) width => container width  
-        $w = array (
-          'xxs' => 450, // container xs
-          'xs' => 720,  // container sm
-          'sm' => 720,  // container sm
-          'md' => 940,  // container md
-          'lg' => 1140  // container lg
-        );
-      }
-      $GLOBALS['TSFE']->register['template_size'] = array(
-        'xxs' => ($w['xxs'] + $gutter) / $maxcols * floatval($xs) - $gutter,
-        'xs' => ($w['xs'] + $gutter) / $maxcols * floatval($xs) - $gutter,
-        'sm' => ($w['sm'] + $gutter) / $maxcols * floatval($sm) - $gutter,
-        'md' => ($w['md'] + $gutter) / $maxcols * floatval($md) - $gutter,
-        'lg' => ($w['lg'] + $gutter) / $maxcols * floatval($lg) - $gutter
-      );
-      return null;
+
+    public function storeToRegister($content = "", $conf = array())
+    {
+
+        $xs = 0;
+        $sm = 0;
+        $md = 0;
+        $lg = 0;
+        $fluid = 0;
+
+        if (isset($conf['fluid.'])) {
+            $fluid = (bool)$this->cObj->stdWrap($conf['fluid'], $conf['fluid.']);
+          }
+
+        if (isset($conf['xs.'])) {
+            $xs = $this->cObj->stdWrap($conf['xs'], $conf['xs.']);
+          }
+
+        if ($xs == 0) {
+            $xs = 12;
+          }
+
+        if (isset($conf['sm.'])) {
+            $sm = $this->cObj->stdWrap($conf['sm'], $conf['sm.']);
+          }
+
+        if ($sm == 0) {
+            $sm = $xs;
+          }
+
+        if (isset($conf['md.'])) {
+            $md = $this->cObj->stdWrap($conf['md'], $conf['md.']);
+          }
+
+        if ($md == 0) {
+            $md = $sm;
+          }
+
+        if (isset($conf['lg.'])) {
+            $lg = $this->cObj->stdWrap($conf['lg'], $conf['lg.']);
+          }
+
+        if ($lg == 0) {
+            $lg = $md;
+          }
+
+        // TODO: put this into settings / plugin config as they cant stay hardcoded
+
+        $gutter = 30;
+        $maxcols = 12;
+
+        // fluid width => next container width
+        if ($fluid) {
+            $w = array (
+                'xxs' => 450, // container xs
+                'xs' => 720,  // container sm
+                'sm' => 940,  // container md
+                'md' => 1140, // container lg
+                'lg' => 1920  // container xl
+                );
+          } else {
+              // fixed (xs always fluid) width => container width
+              $w = array (
+                  'xxs' => 450, // container xs
+                  'xs' => 720,  // container sm
+                  'sm' => 720,  // container sm
+                  'md' => 940,  // container md
+                  'lg' => 1140  // container lg
+                  );
+          }
+        $GLOBALS['TSFE']->register['template_size'] = array(
+            'xxs' => ($w['xxs'] + $gutter) / $maxcols * floatval($xs) - $gutter,
+            'xs' => ($w['xs'] + $gutter) / $maxcols * floatval($xs) - $gutter,
+            'sm' => ($w['sm'] + $gutter) / $maxcols * floatval($sm) - $gutter,
+            'md' => ($w['md'] + $gutter) / $maxcols * floatval($md) - $gutter,
+            'lg' => ($w['lg'] + $gutter) / $maxcols * floatval($lg) - $gutter
+            );
+        return null;
     }
-  }
- ?>
+}
--- Classes/ViewHelpers/ImageSizeViewHelper.php
+++ Classes/ViewHelpers/ImageSizeViewHelper.php
@@ -26,28 +26,31 @@
   use TYPO3\CMS\Fluid\Core\Rendering\RenderingContextInterface;
   use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
   use TYPO3\CMS\Fluid\Core\ViewHelper\Facets\CompilableInterface;
+
   /**
   * @author Benjamin Kott <info@bk2k.info>
   */
-  
-  class ImageSizeViewHelper extends AbstractViewHelper implements CompilableInterface
-  {
 
-    public function initializeArguments() {
-      $this->registerArgument('xs', 'integer', 'Number of columns for extra small', FALSE, 12);
-      $this->registerArgument('sm', 'integer', 'Number of columns for small', FALSE, 0);
-      $this->registerArgument('md', 'integer', 'Number of columns for medium', FALSE, 0);
-      $this->registerArgument('lg', 'integer', 'Number of columns for large', FALSE, 0);
+class ImageSizeViewHelper extends AbstractViewHelper implements CompilableInterface
+{
+
+    public function initializeArguments()
+    {
+        $this->registerArgument('xs', 'integer', 'Number of columns for extra small', false, 12);
+        $this->registerArgument('sm', 'integer', 'Number of columns for small', false, 0);
+        $this->registerArgument('md', 'integer', 'Number of columns for medium', false, 0);
+        $this->registerArgument('lg', 'integer', 'Number of columns for large', false, 0);
     }
 
     public function render()
     {
-      return self::renderStatic(
-        $this->arguments,
-        $this->buildRenderChildrenClosure(),
-        $this->renderingContext
-      );
+        return self::renderStatic(
+            $this->arguments,
+            $this->buildRenderChildrenClosure(),
+            $this->renderingContext
+            );
     }
+
     /**
     * @param array $arguments
     * @param \Closure $renderChildrenClosure
@@ -55,57 +58,56 @@
     * @return void
     */
     public static function renderStatic(
-      array $arguments,
-      \Closure $renderChildrenClosure,
-      RenderingContextInterface $renderingContext
+        array $arguments,
+        \Closure $renderChildrenClosure,
+        RenderingContextInterface $renderingContext
     ) {
-      $templateVariableContainer = $renderingContext->getTemplateVariableContainer();
-      
-      $xs = $arguments['xs'];
-      $sm = $arguments['sm'];
-      if ($sm == 0){
-        $sm = $xs;
-      }
-      $md = $arguments['md'];
-      if ($md == 0){
-        $md = $sm;
-      }
-      $lg = $arguments['lg'];
-      if ($lg == 0){
-        $lg = $md;
-      }
-      
-      // TODO: put defaults in settings     
-      
-      $gutter = 30.0;
-      $maxcols = 12.0;
-      
-      if ($templateVariableContainer->exists('width') === true) {
-        $width =  $templateVariableContainer->get('width');
-        $templateVariableContainer->remove('width');
-      } elseif (isset($GLOBALS['TSFE']->register['template_size'])){
-        $width = $GLOBALS['TSFE']->register['template_size'];
-      } else {
+        $templateVariableContainer = $renderingContext->getTemplateVariableContainer();
+
+        $xs = $arguments['xs'];
+        $sm = $arguments['sm'];
+        if ($sm == 0) {
+            $sm = $xs;
+        }
+        $md = $arguments['md'];
+        if ($md == 0) {
+            $md = $sm;
+        }
+        $lg = $arguments['lg'];
+        if ($lg == 0) {
+            $lg = $md;
+        }
+
         // TODO: put defaults in settings
-        // assume fixed layout (xs always fluid) width => container width  
-        $width = array (
-          'xxs' => 450, // container xs
-          'xs' => 720,  // container sm
-          'sm' => 720,  // container sm
-          'md' => 940,  // container md
-          'lg' => 1140  // container lg
-        );
-      }
-      $newWidth = array (
-        'xxs' => ($width['xxs'] + $gutter) / $maxcols * $xs - $gutter,
-        'xs' => ($width['xs'] + $gutter) / $maxcols * $xs - $gutter,
-        'sm' => ($width['sm'] + $gutter) / $maxcols * $sm - $gutter,
-        'md' => ($width['md'] + $gutter) / $maxcols * $md - $gutter,
-        'lg' => ($width['lg'] + $gutter) / $maxcols * $lg - $gutter
-      );
-      $templateVariableContainer->add('width', $newWidth);
-      
-      return null;
+
+        $gutter = 30.0;
+        $maxcols = 12.0;
+
+        if ($templateVariableContainer->exists('width') === true) {
+            $width =  $templateVariableContainer->get('width');
+            $templateVariableContainer->remove('width');
+          } elseif (isset($GLOBALS['TSFE']->register['template_size'])) {
+            $width = $GLOBALS['TSFE']->register['template_size'];
+          } else {
+            // TODO: put defaults in settings
+            // assume fixed layout (xs always fluid) width => container width
+            $width = array (
+                'xxs' => 450, // container xs
+                'xs' => 720,  // container sm
+                'sm' => 720,  // container sm
+                'md' => 940,  // container md
+                'lg' => 1140  // container lg
+            );
+          }
+        $newWidth = array (
+            'xxs' => ($width['xxs'] + $gutter) / $maxcols * $xs - $gutter,
+            'xs' => ($width['xs'] + $gutter) / $maxcols * $xs - $gutter,
+            'sm' => ($width['sm'] + $gutter) / $maxcols * $sm - $gutter,
+            'md' => ($width['md'] + $gutter) / $maxcols * $md - $gutter,
+            'lg' => ($width['lg'] + $gutter) / $maxcols * $lg - $gutter
+            );
+        $templateVariableContainer->add('width', $newWidth);
+
+        return null;
     }
-  }
-  ?>
+}
