535dccbb8daec7840a0e1b8ac7e40087742eb59d
s-leger <stephen@3dservices.ch>
2016-10-14 22:38:12 +0200

    Refoactory
    Use bk2k:arrayChunk instead of enhanced f:for

---
+++ Classes/ViewHelpers/ArrayChunkViewHelper.php
@@ -0,0 +1,84 @@
+<?php
+namespace BK2K\BootstrapPackage\ViewHelpers;
+
+/*
+ *  The MIT License (MIT)
+ *
+ *  Copyright (c) 2014 Benjamin Kott, http://www.bk2k.info
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
+use TYPO3\CMS\Fluid\Core\ViewHelper\Facets\CompilableInterface;
+
+/**
+ * @author Benjamin Kott <info@bk2k.info>
+ */
+class ArrayChunkViewHelper extends AbstractViewHelper implements CompilableInterface
+{
+
+    /**
+     * Render
+     *
+     * @param string $data
+     * @param string $as
+     * @param integer $size
+     * @return string
+     */
+    public function render($data, $as = 'items', $size = 1)
+    {
+        return self::renderStatic(
+            array(
+                'data' => $data,
+                'as' => $as,
+                'size' => $size
+            ),
+            $this->buildRenderChildrenClosure(),
+            $this->renderingContext
+        );
+    }
+
+    /**
+     * @param array $arguments
+     * @param \Closure $renderChildrenClosure
+     * @param RenderingContextInterface $renderingContext
+     * @return string
+     */
+    public static function renderStatic(
+        array $arguments,
+        \Closure $renderChildrenClosure,
+        RenderingContextInterface $renderingContext
+    ) {
+        $content = '';
+        if (isset($arguments['data'])) {
+            $templateVariableContainer = $renderingContext->getTemplateVariableContainer();
+            $items = array_chunk($arguments['data'], $arguments['size']);
+            if ($templateVariableContainer->exists($arguments['as'])  === true) {
+                $templateVariableContainer->remove($arguments['as']);
+            }
+            $templateVariableContainer->add($arguments['as'], $items);
+            $content = $renderChildrenClosure();
+            $templateVariableContainer->remove($arguments['as']);
+        }
+        return $content;
+    }
+}
