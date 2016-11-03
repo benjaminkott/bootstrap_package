ed63827f06a557b20b06c5c1738e98ee5d30bde8
Benjamin Kott <benjamin.kott@wfp2.com>
2016-11-03 09:46:46 +0100

    [BUGFIX] Use string to identify bootstrap_package for adding static template

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,5 +1,11 @@
 <?php
 defined('TYPO3_MODE') or die();
 
-// Default TypoScript
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Bootstrap Package');
+/***************
+ * Default TypoScript
+ */
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+    'bootstrap_package',
+    'Configuration/TypoScript',
+    'Bootstrap Package'
+);
