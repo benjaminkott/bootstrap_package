a65475ec9547aa1e812c75c145cbfc4b81ce9b0a
Benjamin Kott <benjamin.kott@wfp2.com>
2017-02-14 21:11:09 +0100

    [BUGFIX] Restore location of well and jumbotron in frame class

--- Configuration/TCA/Overrides/tt_content_general_columns.php
+++ Configuration/TCA/Overrides/tt_content_general_columns.php
@@ -14,14 +14,6 @@ $GLOBALS['TCA']['tt_content']['columns']['teaser'] = [
         'rows' => '3'
     ]
 ];
-$GLOBALS['TCA']['tt_content']['columns']['frame_class']['config']['items'][] = [
-    'LLL:EXT:bootstrap_package/Resources/Private/Language/Backend.xlf:field.frame_class.well',
-    'well'
-];
-$GLOBALS['TCA']['tt_content']['columns']['frame_class']['config']['items'][] = [
-    'LLL:EXT:bootstrap_package/Resources/Private/Language/Backend.xlf:field.frame_class.jumbotron',
-    'jumbotron'
-];
 $GLOBALS['TCA']['tt_content']['columns']['imageorient']['config']['items'] = [
     ['LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:imageorient.I.0', 0, 'content-beside-text-img-above-center'],
     ['LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:imageorient.I.3', 8, 'content-beside-text-img-below-center'],
@@ -38,3 +30,27 @@ $GLOBALS['TCA']['tt_content']['columns']['assets'] = [
         $GLOBALS['TYPO3_CONF_VARS']['SYS']['mediafile_ext']
     )
 ];
+
+/***************
+ * Add additional frame class items
+ */
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTcaSelectItem(
+    'tt_content',
+    'frame_class',
+    [
+        'LLL:EXT:bootstrap_package/Resources/Private/Language/Backend.xlf:field.frame_class.well',
+        'well'
+    ],
+    'indent-right',
+    'after'
+);
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTcaSelectItem(
+    'tt_content',
+    'frame_class',
+    [
+        'LLL:EXT:bootstrap_package/Resources/Private/Language/Backend.xlf:field.frame_class.jumbotron',
+        'jumbotron'
+    ],
+    'well',
+    'after'
+);
