4eb7fa6f4a09c4308274d3cad0349d445c690747
Benjamin Kott <benjamin.kott@wfp2.com>
2017-02-14 18:13:35 +0100

    [TASK] Adapt audio element for CMS 8

--- Configuration/TCA/Overrides/tt_content_element_audio.php
+++ Configuration/TCA/Overrides/tt_content_element_audio.php
@@ -4,7 +4,13 @@ defined('TYPO3_MODE') || die();
 /***************
  * Add Content Element: Audio
  */
-$GLOBALS['TCA']['tt_content']['ctrl']['typeicon_classes']['audio'] = 'content-bootstrappackage-audio';
+if (!is_array($GLOBALS['TCA']['tt_content']['types']['audio'])) {
+    $GLOBALS['TCA']['tt_content']['types']['audio'] = [];
+}
+
+/***************
+ * Add content element to seletor list
+ */
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTcaSelectItem(
     'tt_content',
     'CType',
@@ -16,25 +22,38 @@ $GLOBALS['TCA']['tt_content']['ctrl']['typeicon_classes']['audio'] = 'content-bo
     'image',
     'after'
 );
-if (!is_array($GLOBALS['TCA']['tt_content']['types']['audio'])) {
-    $GLOBALS['TCA']['tt_content']['types']['audio'] = [];
-}
+
+/***************
+ * Assign Icon
+ */
+$GLOBALS['TCA']['tt_content']['ctrl']['typeicon_classes']['audio'] = 'content-bootstrappackage-audio';
+
+/***************
+ * Configure element type
+ */
 $GLOBALS['TCA']['tt_content']['types']['audio'] = array_replace_recursive(
     $GLOBALS['TCA']['tt_content']['types']['audio'],
     [
         'showitem' => '
-            --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.general;general,
-            --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.headers;headers,
+            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
+                --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.general;general,
+                --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.headers;headers,
             --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.media,
-            assets,
+                assets,
             --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
                 --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.frames;frames,
                 --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.appearanceLinks;appearanceLinks,
-            --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.access,
-            --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.visibility;visibility,
-            --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.access;access,
-            --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.extended,
-            rowDescription',
+            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,
+                --palette--;;language,
+            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
+                --palette--;;hidden,
+                --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.access;access,
+            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:categories,
+                categories,
+            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:notes,
+                rowDescription,
+            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended,
+        ',
         'columnsOverrides' => [
             'assets' => [
                 'config' => [
