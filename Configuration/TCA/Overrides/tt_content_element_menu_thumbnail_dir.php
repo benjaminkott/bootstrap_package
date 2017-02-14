fefdc5b98d3ef16c8e567e6b149768b44188911d
Benjamin Kott <benjamin.kott@wfp2.com>
2017-02-14 22:53:18 +0100

    [TASK] Migrate thumbnail menus to dedicated menus

---
+++ Configuration/TCA/Overrides/tt_content_element_menu_thumbnail_dir.php
@@ -0,0 +1,59 @@
+<?php
+defined('TYPO3_MODE') || die();
+
+/***************
+ * Add Content Element: Bootstrap Package Panel
+ */
+if (!is_array($GLOBALS['TCA']['tt_content']['types']['menu_thumbnail_dir'])) {
+    $GLOBALS['TCA']['tt_content']['types']['menu_thumbnail_dir'] = [];
+}
+
+/***************
+ * Add content element to seletor list
+ */
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTcaSelectItem(
+    'tt_content',
+    'CType',
+    [
+        'LLL:EXT:bootstrap_package/Resources/Private/Language/Backend.xlf:menu.thumbnail_dir',
+        'menu_thumbnail_dir',
+        'content-bootstrappackage-menu-thumbnail'
+    ],
+    'menu_sitemap_pages',
+    'after'
+);
+
+/***************
+ * Assign Icon
+ */
+$GLOBALS['TCA']['tt_content']['ctrl']['typeicon_classes']['menu_thumbnail_dir'] = 'content-bootstrappackage-menu-thumbnail';
+
+/***************
+ * Configure element type
+ */
+$GLOBALS['TCA']['tt_content']['types']['menu_thumbnail_dir'] = array_replace_recursive(
+    $GLOBALS['TCA']['tt_content']['types']['menu_thumbnail_dir'],
+    [
+        'showitem' => '
+            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
+                --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.general;general,
+                --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.headers;headers,
+                pages;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:pages.ALT.menu_formlabel,
+            --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
+                --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.frames;frames,
+                --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.appearanceLinks;appearanceLinks,
+            --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.accessibility,
+                --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.menu_accessibility;menu_accessibility,
+            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,
+                --palette--;;language,
+            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
+                --palette--;;hidden,
+                --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.access;access,
+            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:categories,
+                categories,
+            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:notes,
+                rowDescription,
+            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended,
+        '
+    ]
+);
