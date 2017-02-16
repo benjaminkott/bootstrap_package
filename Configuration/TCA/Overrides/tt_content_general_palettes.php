dd81e21eae839b85efba886cf4d2eed631d829af
Benjamin Kott <benjamin.kott@wfp2.com>
2017-02-16 23:56:37 +0100

    [TASK] Remove header palette override

--- Configuration/TCA/Overrides/tt_content_general_palettes.php
+++ Configuration/TCA/Overrides/tt_content_general_palettes.php
@@ -16,19 +16,6 @@ $GLOBALS['TCA']['tt_content']['palettes']['mediablock'] = [
         imagecols
     '
 ];
-$GLOBALS['TCA']['tt_content']['palettes']['header'] = [
-    'showitem' => '
-        header;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:header_formlabel,
-        --linebreak--,
-        subheader;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:subheader_formlabel,
-        --linebreak--,
-        header_layout;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:header_layout_formlabel,
-        header_position;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:header_position_formlabel,
-        --linebreak--,
-        header_link;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:header_link_formlabel,
-        --linebreak--
-    '
-];
 $GLOBALS['TCA']['tt_content']['palettes']['header_minimal'] = [
     'canNotCollapse' => 1,
     'showitem' => '
