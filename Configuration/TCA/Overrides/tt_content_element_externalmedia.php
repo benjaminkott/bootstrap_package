e339b4c75025c66eb5a7b7f13518a065f04fc1a8
Benjamin Kott <benjamin.kott@wfp2.com>
2017-04-03 14:50:32 +0200

    [BUGFIX] Use correct external media palette

--- Configuration/TCA/Overrides/tt_content_element_externalmedia.php
+++ Configuration/TCA/Overrides/tt_content_element_externalmedia.php
@@ -38,7 +38,7 @@ $GLOBALS['TCA']['tt_content']['types']['external_media'] = array_replace_recursi
             --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
                 --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.general;general,
                 --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.headers;headers,
-                --palette--;LLL:EXT:bootstrap_package/Resources/Private/Language/Backend.xlf:field.palette.external_media;bootstrap_package_external_media,
+                --palette--;LLL:EXT:bootstrap_package/Resources/Private/Language/Backend.xlf:field.palette.external_media;external_media,
             --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
                 --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.frames;frames,
                 --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.appearanceLinks;appearanceLinks,
