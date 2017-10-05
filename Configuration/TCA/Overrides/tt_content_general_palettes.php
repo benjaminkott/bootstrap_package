cc8328e17b3d56c015cc85f35ef2caec1e989c25
Benjamin Kott <benjamin.kott@wfp2.com>
2017-10-05 17:14:55 +0200

    [TASK] Remove obsolete tt_content palettes

--- Configuration/TCA/Overrides/tt_content_general_palettes.php
+++ Configuration/TCA/Overrides/tt_content_general_palettes.php
@@ -4,18 +4,6 @@ defined('TYPO3_MODE') || die();
 /***************
  * Add Palettes for Generic usage
  */
-$GLOBALS['TCA']['tt_content']['palettes']['imageblock'] = [
-    'showitem' => '
-        imageorient,
-        imagecols
-    '
-];
-$GLOBALS['TCA']['tt_content']['palettes']['mediablock'] = [
-    'showitem' => '
-        imageorient,
-        imagecols
-    '
-];
 $GLOBALS['TCA']['tt_content']['palettes']['header_minimal'] = [
     'showitem' => '
         header;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:header_formlabel,
