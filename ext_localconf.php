ddd4880bb224f529020cee1f6545b5613f1d3ea2
Benjamin Kott <benjamin.kott@wfp2.com>
2017-10-26 13:19:06 +0200

    [TASK] Streamline php header comments and add fixer rule

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,7 +1,13 @@
 <?php
-if (!defined('TYPO3_MODE')) {
-    die('Access denied.');
-}
+
+/*
+ * This file is part of the package bk2k/bootstrap-package.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+defined('TYPO3_MODE') || die();
 
 /***************
  * Define TypoScript as content rendering template
