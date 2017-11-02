a9bd8c59a2ea6828ffa4db8951edab69368ab84a
Benjamin Kott <benjamin.kott@wfp2.com>
2017-11-02 16:02:02 +0100

    [FEATURE] Add background image and base coloring support for content elements

--- Configuration/TCA/Overrides/tt_content_general_columns.php
+++ Configuration/TCA/Overrides/tt_content_general_columns.php
@@ -12,6 +12,71 @@ defined('TYPO3_MODE') || die();
 /***************
  * Adjust columns for generic usage
  */
+$GLOBALS['TCA']['tt_content']['columns']['background_image'] = [
+    'exclude' => true,
+    'displayCond' => 'FIELD:frame_class:!=:none',
+    'label' => 'LLL:EXT:bootstrap_package/Resources/Private/Language/Backend.xlf:field.background_image',
+    'config' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::getFileFieldTCAConfig(
+        'background_image',
+        [
+            'appearance' => [
+                'createNewRelationLinkTitle' => 'LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:images.addFileReference'
+            ],
+            'overrideChildTca' => [
+                'types' => [
+                    \TYPO3\CMS\Core\Resource\File::FILETYPE_UNKNOWN => [
+                        'showitem' => '
+                            --palette--;;filePalette
+                        '
+                    ],
+                    \TYPO3\CMS\Core\Resource\File::FILETYPE_TEXT => [
+                        'showitem' => '
+                            --palette--;;filePalette
+                        '
+                    ],
+                    \TYPO3\CMS\Core\Resource\File::FILETYPE_IMAGE => [
+                        'showitem' => '
+                            --palette--;;filePalette
+                        '
+                    ],
+                    \TYPO3\CMS\Core\Resource\File::FILETYPE_AUDIO => [
+                        'showitem' => '
+                            --palette--;;filePalette
+                        '
+                    ],
+                    \TYPO3\CMS\Core\Resource\File::FILETYPE_VIDEO => [
+                        'showitem' => '
+                            --palette--;;filePalette
+                        '
+                    ],
+                    \TYPO3\CMS\Core\Resource\File::FILETYPE_APPLICATION => [
+                        'showitem' => '
+                            --palette--;;filePalette
+                        '
+                    ],
+                ],
+            ],
+            'minitems' => 0,
+            'maxitems' => 1,
+        ],
+        $GLOBALS['TYPO3_CONF_VARS']['GFX']['imagefile_ext']
+    ),
+];
+$GLOBALS['TCA']['tt_content']['columns']['background_color_class'] = [
+    'exclude' => true,
+    'displayCond' => 'FIELD:frame_class:!=:none',
+    'label' => 'LLL:EXT:bootstrap_package/Resources/Private/Language/Backend.xlf:field.background_color_class',
+    'config' => [
+        'type' => 'select',
+        'renderType' => 'selectSingle',
+        'items' => [
+            ['none', 'none'],
+            ['primary', 'primary'],
+            ['light', 'light'],
+            ['dark', 'dark']
+        ]
+    ],
+];
 $GLOBALS['TCA']['tt_content']['columns']['teaser'] = [
     'label' => 'LLL:EXT:bootstrap_package/Resources/Private/Language/Backend.xlf:field.teaser',
     'exclude' => true,
@@ -45,6 +110,21 @@ $GLOBALS['TCA']['tt_content']['columns']['tx_bootstrappackage_carousel_item'] = 
         ]
     ]
 ];
+
+/***************
+ * Adjust default fields
+ */
+$GLOBALS['TCA']['tt_content']['columns']['frame_class']['onChange'] = 'reload';
+
+/***************
+ * Add fields to default palettes
+ */
+$GLOBALS['TCA']['tt_content']['palettes']['frames']['showitem'] .= '
+    --linebreak--,
+    background_image,
+    --linebreak--,
+    background_color_class
+';
 
 /***************
  * Add additional frame class items
--- ext_tables.sql
+++ ext_tables.sql
@@ -25,6 +25,8 @@ CREATE TABLE tt_content (
     tx_bootstrappackage_carousel_item int(11) unsigned DEFAULT '0',
     tx_bootstrappackage_accordion_item int(11) unsigned DEFAULT '0',
     tx_bootstrappackage_tab_item int(11) unsigned DEFAULT '0',
+    background_color_class varchar(255) DEFAULT '' NOT NULL,
+    background_image int(11) unsigned DEFAULT '0',
 );
 
 
