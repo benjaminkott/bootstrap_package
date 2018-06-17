abaac5c9d047be6be44c579ccb43ec39ad6d2b64
Benjamin Kott <benjamin.kott@wfp2.com>
2018-06-17 23:42:13 +0200

    [TASK] Move render hook registration

--- ext_localconf.php
+++ ext_localconf.php
@@ -120,18 +120,18 @@ if (TYPO3_MODE === 'BE') {
 }
 
 /***************
+ * Register google font hook
+ */
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-preProcess'][]
+    = 'BK2K\\BootstrapPackage\\Hooks\\PageRenderer\\GoogleFontHook->execute';
+
+/***************
  * Register css processing parser
  */
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/bootstrap-package/css']['parser'][] =
     \BK2K\BootstrapPackage\Parser\ScssParser::class;
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/bootstrap-package/css']['parser'][] =
     \BK2K\BootstrapPackage\Parser\LessParser::class;
-
-/***************
- * Register google font hook
- */
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-preProcess'][]
-    = 'BK2K\\BootstrapPackage\\Hooks\\PageRenderer\\GoogleFontHook->execute';
 
 /***************
  * Register css processing hooks
