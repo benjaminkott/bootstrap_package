9a84c8280f2f5a55ff89235fb848c33b3fb09104
Benjamin Kott <benjamin.kott@wfp2.com>
2018-06-18 15:52:19 +0200

    [TASK] Use modern user agent to fetch data

--- Classes/Service/GoogleFontService.php
+++ Classes/Service/GoogleFontService.php
@@ -49,7 +49,11 @@ class GoogleFontService
      */
     public function cacheFile($file)
     {
-        $content = GeneralUtility::getUrl($file);
+        $content = GeneralUtility::getUrl(
+            $file,
+            0,
+            ['User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/17.17134']
+        );
 
         // Ensure cache directory exists
         GeneralUtility::mkdir_deep($this->tempDirectory . '/' . $this->getCacheIdentifier($file));
