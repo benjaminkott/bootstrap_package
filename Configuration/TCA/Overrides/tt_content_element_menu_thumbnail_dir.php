5331a4c92ca3770ca3f5103c8ebabae027b6a9d7
Benjamin Kott <benjamin.kott@outlook.com>
2018-09-21 11:57:31 +0200

    [!!!][FEATURE] Provide new design for thumbnail menu content element
    The thumbnail content elements was kind out of place after card menu was introduced.
    We are now refreshing the usecase of the thumbnail menu to have a more unique
    look and feel to better coexist with the card menu.

--- Configuration/TCA/Overrides/tt_content_element_menu_thumbnail_dir.php
+++ Configuration/TCA/Overrides/tt_content_element_menu_thumbnail_dir.php
@@ -56,6 +56,8 @@ $GLOBALS['TCA']['tt_content']['types']['menu_thumbnail_dir'] = array_replace_rec
                 --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.general;general,
                 --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.headers;headers,
                 pages;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:pages.ALT.menu_formlabel,
+            --div--;LLL:EXT:bootstrap_package/Resources/Private/Language/Backend.xlf:menu.thumbnail.options,
+                pi_flexform;LLL:EXT:bootstrap_package/Resources/Private/Language/Backend.xlf:advanced,
             --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
                 --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.frames;frames,
                 --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.appearanceLinks;appearanceLinks,
@@ -73,4 +75,13 @@ $GLOBALS['TCA']['tt_content']['types']['menu_thumbnail_dir'] = array_replace_rec
             --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended,
         '
     ]
+);
+
+/***************
+ * Add flexForms for content element configuration
+ */
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+    '*',
+    'FILE:EXT:bootstrap_package/Configuration/FlexForms/MenuThumbnail.xml',
+    'menu_thumbnail_dir'
 );
