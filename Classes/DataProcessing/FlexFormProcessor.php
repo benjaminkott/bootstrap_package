6c8395e50f46d1f9d9e1f09f5c4a73a54da709ff
Oliver Hader <oliver@typo3.org>
2018-12-27 11:24:14 +0100

    [TASK] Adjust to use Core's FlexFormService directly

--- Classes/DataProcessing/FlexFormProcessor.php
+++ Classes/DataProcessing/FlexFormProcessor.php
@@ -10,8 +10,8 @@ declare(strict_types = 1);
 
 namespace BK2K\BootstrapPackage\DataProcessing;
 
+use TYPO3\CMS\Core\Service\FlexFormService;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Extbase\Service\FlexFormService;
 use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
 use TYPO3\CMS\Frontend\ContentObject\DataProcessorInterface;
 
