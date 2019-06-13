86b9e094f9d0f724eede1fb021f44c8038a4a458
Benjamin Kott <benjamin.kott@outlook.com>
2019-06-13 17:47:57 +0200

    [FEATURE] Allow processing of configured file types
    To deliver the best results possible, the bootstrap package
    has a strict type checking before passing assets to the
    correct renderings. Even if your system was capable of
    rendering images from PDFs it was not allowed by default
    through those type checkings, since a PDF is in the
    mimetype group application. That means we were not able
    to simply allow these application group since that would
    also mean that the system would try to generate a image
    from a zip file, like in fluid styled content.
    
    Since there is still a huge demand for PDF previews, we
    made the decision to extend the current typechecks. TYPO3
    allows you to configure imagefile_ext and mediafile_ext in
    its configuration. We are now also allowing the processing
    of files with extensions configured in the according lists.
    
    Fixes: #647
    Fixes: #576

---
+++ Classes/ViewHelpers/File/IsAudioViewHelper.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+ * This file is part of the package bk2k/bootstrap-package.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace BK2K\BootstrapPackage\ViewHelpers\File;
+
+use TYPO3\CMS\Core\Resource\File;
+use TYPO3\CMS\Core\Resource\FileReference;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\CompileWithRenderStatic;
+
+class IsAudioViewHelper extends AbstractViewHelper
+{
+    use CompileWithRenderStatic;
+
+    /**
+     * @param array $arguments
+     * @param \Closure $renderChildrenClosure
+     * @param RenderingContextInterface $renderingContext
+     * @return mixed
+     */
+    public static function renderStatic(
+        array $arguments,
+        \Closure $renderChildrenClosure,
+        RenderingContextInterface $renderingContext
+    ) {
+        $file = $renderChildrenClosure();
+        $allowedFileExtensions = $GLOBALS['TYPO3_CONF_VARS']['SYS']['audiofile_ext'] ?? '';
+        $allowedFileExtensions = GeneralUtility::trimExplode(',', $allowedFileExtensions);
+
+        if (is_object($file)
+            && in_array(get_class($file), [FileReference::class, File::class], true)
+            && (
+                in_array($file->getExtension(), $allowedFileExtensions, true)
+                || $file->getType() === File::FILETYPE_AUDIO
+            )
+        ) {
+            return true;
+        }
+
+        return false;
+    }
+}
