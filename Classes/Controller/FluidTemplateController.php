fe775e75e665584e2ebf12d54cc6d15ca7b8f94c
Benjamin Kott <benjamin.kott@outlook.com>
2019-10-25 15:28:27 +0200

    Add pagination

---
+++ Classes/Controller/FluidTemplateController.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types = 1);
+
+/*
+ * This file is part of the package bk2k/bootstrap-package.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace T3G\AgencyPack\Blog\Controller;
+
+use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
+
+class ContentController extends ActionController
+{
+    public function genericAction(): void
+    {
+    }
+}
