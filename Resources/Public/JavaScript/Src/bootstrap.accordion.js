4c0bff4de3986ebe9553eb89097417ede7aef220
jonaseberle <flightvision@googlemail.com>
2019-11-12 22:55:24 +0100

    [BUGFIX] Resolve plain bootstrap 4 accordion conflicts (#775)

--- Resources/Public/JavaScript/Src/bootstrap.accordion.js
+++ Resources/Public/JavaScript/Src/bootstrap.accordion.js
@@ -2,11 +2,15 @@
  * Scroll to top of collapsed/expanded accordion item
  */
 $('.accordion').on('hide.bs.collapse', function (e) {
-    var headingTop = $(e.target).prev('.accordion-header').offset().top - 5;
-    var visibleTop = $(window).scrollTop();
-    if (headingTop < visibleTop) {
-        $('html,body').animate({
-            scrollTop: headingTop
-        }, 500);
+    var $accordionHeader = $(e.target).prev('.accordion-header')
+    if ($accordionHeader.length > 0) {
+        var headingTop = $accordionHeader.offset().top - 5;
+        var visibleTop = $(window).scrollTop();
+        if (headingTop < visibleTop) {
+            $('html,body').animate({
+                scrollTop: headingTop
+            }, 500);
+        }
     }
 });
+
