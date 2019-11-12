1d4557e5576154fba10dbb02e88963b96377aac7
Benjamin Kott <benjamin.kott@outlook.com>
2019-11-12 22:47:03 +0100

    [BUGFIX] Correct check for element existence

--- Resources/Public/JavaScript/Src/bootstrap.accordion.js
+++ Resources/Public/JavaScript/Src/bootstrap.accordion.js
@@ -3,7 +3,7 @@
  */
 $('.accordion').on('hide.bs.collapse', function (e) {
     var $accordionHeader = $(e.target).prev('.accordion-header')
-    if ($accordionHeader) {
+    if ($accordionHeader.length > 0) {
         var headingTop = $accordionHeader.offset().top - 5;
         var visibleTop = $(window).scrollTop();
         if (headingTop < visibleTop) {
