158b6e3a34cca56174178ae1cedfdc73bbff41fd
MartinH0 <martinhotmann@gmail.com>
2019-11-16 12:36:39 +0100

    Updated Header Eventlistener

--- Resources/Public/JavaScript/Src/bootstrap.stickyheader.js
+++ Resources/Public/JavaScript/Src/bootstrap.stickyheader.js
@@ -1,15 +1,10 @@
-$(function animateHeader() {
-
-    $(window).on('resize, scroll', function() {
-        var stickyheaderScrolled = $(window).scrollTop();
-        if (stickyheaderScrolled > 150) {
-            $('.navbar-fixed-top').addClass('navbar-transition');
-        } else {
-            $('.navbar-fixed-top').removeClass('navbar-transition');
-        }
-    });
-  	document.addEventListener("DOMContentLoaded", function(n) {
-    	animateHeader()
-	})
-
+function animateHeader() {
+    150<$(window).scrollTop() ? $(".navbar-fixed-top").addClass("navbar-transition") : $(".navbar-fixed-top").removeClass("navbar-transition")
+}
+document.addEventListener("DOMContentLoaded", function(n) {
+    animateHeader()
+}), $(function() {
+    $(window).on("resize, scroll", function() {
+        animateHeader()
+    })
 });
