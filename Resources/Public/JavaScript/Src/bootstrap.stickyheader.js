72920acf3b17136a2aecbc6fa78d872466f1474c
MartinH0 <martinhotmann@gmail.com>
2019-11-18 15:02:54 +0100

    Update bootstrap.stickyheader.js

--- Resources/Public/JavaScript/Src/bootstrap.stickyheader.js
+++ Resources/Public/JavaScript/Src/bootstrap.stickyheader.js
@@ -1,7 +1,7 @@
 var navelem = document.getElementsByClassName("navbar-fixed-top");
 function animateHeader() {
-	150<window.scrollY ? navelem[0].classList.add("navbar-transition") : navelem[0].classList.remove("navbar-transition")
-};
+	150<window.scrollY ? navelem[0].classList.add("navbar-transition") : navelem[0].classList.remove("navbar-transition");
+}
 ['scroll', 'resize', 'DOMContentLoaded'].forEach(function(e) {
   window.addEventListener(e, animateHeader);
 });
