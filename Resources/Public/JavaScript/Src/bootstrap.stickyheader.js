512196b8aafbf8d8fcb1831b29991edbe49d6472
MartinH0 <martinhotmann@gmail.com>
2019-11-18 14:47:30 +0100

    renamed "element" to "navelem" because of conflict

--- Resources/Public/JavaScript/Src/bootstrap.stickyheader.js
+++ Resources/Public/JavaScript/Src/bootstrap.stickyheader.js
@@ -1,6 +1,6 @@
-let element = document.getElementsByClassName("navbar-fixed-top");
+let navelem = document.getElementsByClassName("navbar-fixed-top");
 function animateHeader() {
-	150<window.scrollY ? element[0].classList.add("navbar-transition") : element[0].classList.remove("navbar-transition")
+	150<window.scrollY ? navelem[0].classList.add("navbar-transition") : navelem[0].classList.remove("navbar-transition")
 };
 ['scroll', 'resize', 'DOMContentLoaded'].forEach(function(e) {
   window.addEventListener(e, animateHeader);
