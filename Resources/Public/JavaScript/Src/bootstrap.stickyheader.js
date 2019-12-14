0109d3b0904b66258ba72d983799e8c7562cce90
Benjamin Kott <benjamin.kott@outlook.com>
2019-12-14 18:18:14 +0100

    [BUGFIX] Ensure elements exist before accessing
    Fixes #834

--- Resources/Public/JavaScript/Src/bootstrap.stickyheader.js
+++ Resources/Public/JavaScript/Src/bootstrap.stickyheader.js
@@ -1,7 +1,9 @@
-var stickyheader = document.getElementsByClassName("navbar-fixed-top");
-function animateHeader() {
-	150<window.scrollY ? stickyheader[0].classList.add("navbar-transition") : stickyheader[0].classList.remove("navbar-transition");
+var stickyheader = document.querySelectorAll(".navbar-fixed-top");
+if (stickyheader.length >= 1) {
+    function animateHeader() {
+        150 < window.scrollY ? stickyheader[0].classList.add("navbar-transition") : stickyheader[0].classList.remove("navbar-transition");
+    }
+    ['scroll', 'resize', 'DOMContentLoaded'].forEach(function(e) {
+        window.addEventListener(e, animateHeader);
+    });
 }
-['scroll', 'resize', 'DOMContentLoaded'].forEach(function(e) {
-  window.addEventListener(e, animateHeader);
-});
