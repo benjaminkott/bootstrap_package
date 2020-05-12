260de628e14ba0e648d15989458afece900b5261
Gilbertsoft <25326036+gilbertsoft@users.noreply.github.com>
2020-05-12 23:35:51 +0200

    [TASK] Rename variables and optimize path calculations

--- Classes/Parser/ScssParser.php
+++ Classes/Parser/ScssParser.php
@@ -117,21 +117,29 @@ class ScssParser extends AbstractParser
 
         // Make paths in url() statements relative to site root
         $absoluteFilePath = dirname($absoluteFilename);
-        $siteFilePath = dirname(substr($absoluteFilename, strlen($this->getPathSite())));
-        $absoluteBootstrapPackagePath = GeneralUtility::getFileAbsFileName(ExtensionManagementUtility::extPath('bootstrap_package') . 'Resources/Public/Scss/Theme');
-        $siteBootstrapPackagePath = substr($absoluteBootstrapPackagePath, strlen($this->getPathSite()));
+        $relativeFilePath = PathUtility::getAbsoluteWebPath($absoluteFilePath);
+        $absoluteBootstrapPackageThemePath =
+            ExtensionManagementUtility::extPath('bootstrap_package') . 'Resources/Public/Scss/Theme';
+        $relativeBootstrapPackageThemePath = PathUtility::getAbsoluteWebPath($absoluteBootstrapPackageThemePath);
         $scss->registerFunction(
             'url',
-            function ($args) use ($scss, $absoluteFilePath, $siteFilePath, $absoluteBootstrapPackagePath, $siteBootstrapPackagePath) {
+            function ($args) use (
+                $scss,
+                $absoluteFilePath,
+                $relativeFilePath,
+                $absoluteBootstrapPackageThemePath,
+                $relativeBootstrapPackageThemePath
+            ) {
                 $marker = $args[0][1];
                 $args[0][1] = '';
                 $result = $scss->compileValue($args[0]);
                 if (substr_compare($result, 'data:', 0, 5, true) !== 0) {
                     if (is_file(PathUtility::getCanonicalPath($absoluteFilePath . '/' . $result))) {
-                        $result = PathUtility::getCanonicalPath($siteFilePath . '/' . $result);
-                    } elseif (is_file(PathUtility::getCanonicalPath($absoluteBootstrapPackagePath . '/' . $result))) {
-                        $result = PathUtility::getCanonicalPath($siteBootstrapPackagePath . '/' . $result);
+                        $result = PathUtility::getCanonicalPath($relativeFilePath . '/' . $result);
+                    } elseif (is_file(PathUtility::getCanonicalPath($absoluteBootstrapPackageThemePath . '/' . $result))) {
+                        $result = PathUtility::getCanonicalPath($relativeBootstrapPackageThemePath . '/' . $result);
                     }
+                    $result = substr($result, 0, 1) === '/' ? substr($result, 1) : $result;
                 }
                 return 'url(' . $marker . $result . $marker . ')';
             }
@@ -140,10 +148,10 @@ class ScssParser extends AbstractParser
         // Compile file
         $css = $scss->compile('@import "' . $absoluteFilename . '"');
 
-        // Fix url() statements
-        $relativePath = $settings['cache']['tempDirectoryRelativeToRoot'] . dirname($settings['file']['relative']) . '/';
+        // Fix paths in url() statements to be relative to temp directory
+        $relativeTempPath = $settings['cache']['tempDirectoryRelativeToRoot'];
         $search = '%url\s*\(\s*[\\\'"]?(?!(((?:https?:)?\/\/)|(?:data:?:)))([^\\\'")]+)[\\\'"]?\s*\)%';
-        $replace = 'url("' . $relativePath . '$3")';
+        $replace = 'url("' . $relativeTempPath . '$3")';
         $css = preg_replace($search, $replace, $css);
 
         return [
