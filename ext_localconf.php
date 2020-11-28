cf8b051b0db7ea2a7ab2fa3c2e9d7abd2e0dc87b
Simon Gilli <25326036+gilbertsoft@users.noreply.github.com>
2020-11-28 23:16:36 +0100

    [TASK] Rename constant for file protection
    See https://review.typo3.org/c/Packages/TYPO3.CMS/+/66937

--- ext_localconf.php
+++ ext_localconf.php
@@ -7,7 +7,7 @@
  * LICENSE file that was distributed with this source code.
  */
 
-defined('TYPO3_MODE') || die();
+defined('TYPO3') || die();
 
 /***************
  * Define TypoScript as content rendering template
