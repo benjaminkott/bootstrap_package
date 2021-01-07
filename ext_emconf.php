77488690621df9c9790a4b79ce9a8724ed54e410
Benjamin Kott <benjamin.kott@outlook.com>
2021-01-07 14:07:16 +0100

    [TASK] Allow installation on TYPO3 v11 for development purposes

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,9 +13,9 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'templates',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-10.4.99',
-            'rte_ckeditor' => '9.5.0-10.4.99',
-            'seo' => '9.5.0-10.4.99'
+            'typo3' => '9.5.0-11.0.99',
+            'rte_ckeditor' => '9.5.0-11.0.99',
+            'seo' => '9.5.0-11.0.99'
         ],
         'conflicts' => [
             'css_styled_content' => '*',
