f546ad4b3f72ba2ee4612fde7bdb0e16344de177
Benjamin Kott <benjamin.kott@outlook.com>
2021-05-21 12:55:28 +0200

    [TASK] Cleanups Part 8

--- Classes/Updates/CarouselItemTypeUpdate.php
+++ Classes/Updates/CarouselItemTypeUpdate.php
@@ -115,10 +115,10 @@ class CarouselItemTypeUpdate implements UpgradeWizardInterface, RepeatableInterf
     }
 
     /**
-     * @param int $type
+     * @param string $type
      * @return string
      */
-    protected function mapValues($type)
+    protected function mapValues(string $type)
     {
         $mapping = [
             'textandimage' => 'text_and_image',
