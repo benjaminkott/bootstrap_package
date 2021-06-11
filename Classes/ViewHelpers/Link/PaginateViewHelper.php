f56575584a9f6d761bac3949bd37b23084aed1b7
Benjamin Kott <benjamin.kott@outlook.com>
2021-06-11 13:06:02 +0200

    [!!!][TASK] Replace fluid pagination widget (#1012)
    The pagination widget view helper was removed from TYPO3 in v11.
    To make migration easier we are introducing a new pagination viewhelper 
    that utilizes the native typo3 pagination that was introduced in v10. 
    
    For now, overwriting the templates for pagination is not supported 
    until we feel this is stable enough.
    
    ## Further Reading
    Introduction of Pagination:
    https://docs.typo3.org/c/typo3/cms-core/master/en-us/Changelog/10.2/Feature-89603-IntroduceNativePaginationForLists.html
    
    Removal of Fluid Widgets:
    https://docs.typo3.org/c/typo3/cms-core/master/en-us/Changelog/11.0/Breaking-92529-AllFluidWidgetFunctionalityRemoved.html
    
    Closes #1009

---
+++ Classes/ViewHelpers/Link/PaginateViewHelper.php
@@ -0,0 +1,61 @@
+<?php
+
+/*
+ * This file is part of the package bk2k/bootstrap-package.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace BK2K\BootstrapPackage\ViewHelpers\Link;
+
+use TYPO3\CMS\Extbase\Mvc\Web\Routing\UriBuilder;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractTagBasedViewHelper;
+
+class PaginateViewHelper extends AbstractTagBasedViewHelper
+{
+    /**
+     * @var string
+     */
+    protected $tagName = 'a';
+
+    /**
+     * Initialize arguments
+     */
+    public function initializeArguments()
+    {
+        parent::initializeArguments();
+        $this->registerUniversalTagAttributes();
+
+        $this->registerArgument('paginationId', 'string', 'id', true);
+        $this->registerArgument('paginationPage', 'int', 'page', true);
+    }
+
+    public function render(): string
+    {
+        $paginationId = (string) $this->arguments['paginationId'];
+        $paginationPage = (int) $this->arguments['paginationPage'];
+        $section = isset($this->arguments['section']) ? (string)$this->arguments['section'] : '';
+
+        $arguments = [];
+        if ($paginationPage > 1) {
+            $arguments['paginate'][$paginationId]['page'] = $paginationPage;
+        }
+
+        /** @var UriBuilder $uriBuilder */
+        $uriBuilder = $this->renderingContext->getUriBuilder();
+        $uriBuilder->reset()->setArguments($arguments);
+
+        $uri = $uriBuilder->build();
+        if ($uri !== '') {
+            $this->tag->addAttribute('href', $uri);
+            $this->tag->setContent($this->renderChildren());
+            $this->tag->forceClosingTag(true);
+            $result = $this->tag->render();
+        } else {
+            $result = $this->renderChildren();
+        }
+
+        return $result;
+    }
+}
