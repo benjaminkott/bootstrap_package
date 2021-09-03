600372b95cad394c63d1cc94a9ee0feff9df1bc1
Benjamin Kott <benjamin.kott@outlook.com>
2021-09-03 11:53:20 +0200

    [FEATURE] Introduce frame options
    To provide more flexibility we introduce
    frame options to replace most of the existing
    options of frame classes. In contrast to the
    hard selection of one option like ruler-before
    or ruler-after, the frame options allow to
    combine those.
    
    The option indent was dropped completly
    and is now a combination of indent-left
    and indent-right.
    
    Adding new options
    
    ```
    TCEFORM {
        tt_content {
            frame_options {
                addItems {
                    coolstyle = New cool style
                }
            }
        }
    }
    ```
    
    All options will be prefixed with frame-option-* in the output.

---
+++ Classes/Updates/FrameClassToOptionsUpdate.php
@@ -0,0 +1,152 @@
+<?php
+declare(strict_types=1);
+
+/*
+ * This file is part of the package bk2k/bootstrap-package.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace BK2K\BootstrapPackage\Updates;
+
+use TYPO3\CMS\Core\Database\Connection;
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Database\Query\Restriction\DeletedRestriction;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Install\Updates\DatabaseUpdatedPrerequisite;
+use TYPO3\CMS\Install\Updates\RepeatableInterface;
+use TYPO3\CMS\Install\Updates\UpgradeWizardInterface;
+
+/**
+ * FrameClassToOptionsUpdate
+ */
+class FrameClassToOptionsUpdate implements UpgradeWizardInterface, RepeatableInterface
+{
+    /**
+     * @var string
+     */
+    protected $table = 'tt_content';
+
+    /**
+     * @var string
+     */
+    protected $field = 'frame_class';
+
+    /**
+     * @var array
+     */
+    protected $mapping = [
+        'ruler-before' => 'ruler-before',
+        'ruler-after' => 'ruler-after',
+        'indent' => 'indent-left,indent-right',
+        'indent-left' => 'indent-left',
+        'indent-right' => 'indent-right'
+    ];
+
+    /**
+     * @return string
+     */
+    public function getIdentifier(): string
+    {
+        return self::class;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return '[Bootstrap Package] Migrate obsolete frame_class options to frame options';
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getPrerequisites(): array
+    {
+        return [
+            DatabaseUpdatedPrerequisite::class
+        ];
+    }
+
+    /**
+     * @return bool
+     */
+    public function updateNecessary(): bool
+    {
+        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable($this->table);
+        $queryBuilder->getRestrictions()->removeAll()->add(GeneralUtility::makeInstance(DeletedRestriction::class));
+        $elementCount = $queryBuilder->count('uid')
+            ->from($this->table)
+            ->where(
+                $queryBuilder->expr()->in(
+                    $this->field,
+                    $queryBuilder->createNamedParameter(
+                        array_keys($this->mapping),
+                        Connection::PARAM_STR_ARRAY
+                    )
+                )
+            )
+            ->execute()->fetchColumn(0);
+        return (bool)$elementCount;
+    }
+
+    /**
+     * @return bool
+     */
+    public function executeUpdate(): bool
+    {
+        $connection = GeneralUtility::makeInstance(ConnectionPool::class)->getConnectionForTable($this->table);
+        $queryBuilder = $connection->createQueryBuilder();
+        $queryBuilder->getRestrictions()->removeAll()->add(GeneralUtility::makeInstance(DeletedRestriction::class));
+        $statement = $queryBuilder->select('uid', $this->field)
+            ->from($this->table)
+            ->where(
+                $queryBuilder->expr()->in(
+                    $this->field,
+                    $queryBuilder->createNamedParameter(
+                        array_keys($this->mapping),
+                        Connection::PARAM_STR_ARRAY
+                    )
+                )
+            )
+            ->execute();
+        while ($record = $statement->fetch()) {
+            if (null !== $newValue = $this->mapValues($record[$this->field])) {
+                $queryBuilder = $connection->createQueryBuilder();
+                $queryBuilder->update($this->table)
+                    ->where(
+                        $queryBuilder->expr()->eq(
+                            'uid',
+                            $queryBuilder->createNamedParameter($record['uid'], \PDO::PARAM_INT)
+                        )
+                    )
+                    ->set($this->field, 'default')
+                    ->set('frame_options', $newValue);
+                $queryBuilder->execute();
+            }
+        }
+        return true;
+    }
+
+    /**
+     * @param string|int $value
+     * @return string|null
+     */
+    protected function mapValues($value)
+    {
+        if (array_key_exists($value, $this->mapping)) {
+            return $this->mapping[$value];
+        }
+        return null;
+    }
+}
