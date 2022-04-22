315ad3101dd36f1817502f505a0c1d0c48b8cc09
Benjamin Kott <benjamin.kott@outlook.com>
2022-04-22 11:23:52 +0200

    [!!!][TASK] Drop v10 support

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,9 +13,9 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'templates',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-11.5.99',
-            'rte_ckeditor' => '10.4.0-11.5.99',
-            'seo' => '10.4.0-11.5.99'
+            'typo3' => '11.5.0-12.0.0',
+            'rte_ckeditor' => '11.5.0-12.0.0',
+            'seo' => '11.5.0-12.0.0'
         ],
         'conflicts' => [
             'css_styled_content' => '*',
