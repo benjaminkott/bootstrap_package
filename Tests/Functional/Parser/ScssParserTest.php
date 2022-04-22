24167a709d2832c4beb4399fec5a087bf83ef90d
Benjamin Kott <benjamin.kott@outlook.com>
2022-04-22 08:19:58 +0200

    [TASK] Adapt PHPDoc annotation in ScssParserTest

--- Tests/Functional/Parser/ScssParserTest.php
+++ Tests/Functional/Parser/ScssParserTest.php
@@ -21,7 +21,7 @@ use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
 class ScssParserTest extends FunctionalTestCase
 {
     /**
-     * @var array
+     * @var string[]
      */
     protected $testExtensionsToLoad = [
         'typo3conf/ext/bootstrap_package',
