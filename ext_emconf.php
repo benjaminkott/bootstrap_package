c5b078c56cc9e2792c6653f50d7461547d01202a
Benjamin Kott <benjamin.kott@outlook.com>
2023-04-24 11:50:49 +0200

    [TASK] Allow 12.4 in ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,9 +13,9 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'templates',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-12.1.99',
-            'rte_ckeditor' => '11.5.0-12.1.99',
-            'seo' => '11.5.0-12.1.99'
+            'typo3' => '11.5.0-12.4.99',
+            'rte_ckeditor' => '11.5.0-12.4.99',
+            'seo' => '11.5.0-12.4.99'
         ],
         'conflicts' => [
             'css_styled_content' => '*',
