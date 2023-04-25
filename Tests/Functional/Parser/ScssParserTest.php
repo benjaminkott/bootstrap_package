e5e9dcc7dca2eb7bfe98a26d02470daadd509f0f
Benjamin Kott <benjamin.kott@outlook.com>
2023-04-25 16:52:17 +0200

    gsidfjg

--- Tests/Functional/Parser/ScssParserTest.php
+++ Tests/Functional/Parser/ScssParserTest.php
@@ -20,6 +20,13 @@ use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
  */
 class ScssParserTest extends FunctionalTestCase
 {
+    protected bool $initializeDatabase = false;
+
+    protected array $coreExtensionsToLoad = [
+        'seo',
+        'rte_ckeditor',
+    ];
+
     protected array $testExtensionsToLoad = [
         'typo3conf/ext/bootstrap_package',
         'typo3conf/ext/demo_package',
