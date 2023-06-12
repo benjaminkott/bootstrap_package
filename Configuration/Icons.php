e8ed72955127d3f4036e1b131939ebee96f73372
Sven Jürgens <SvenJuergens@users.noreply.github.com>
2023-06-12 13:08:37 +0200

    [TASK] Move Registering of Icons to ServiceContainer  (#1374)

---
+++ Configuration/Icons.php
@@ -0,0 +1,127 @@
+<?php
+
+/*
+ * This file is part of the package bk2k/bootstrap-package.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+return [
+    'systeminformation-bootstrappackage' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:bootstrap_package/Resources/Public/Icons/SystemInformation/bootstrappackage.svg'
+    ],
+    'content-bootstrappackage-accordion' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:bootstrap_package/Resources/Public/Icons/ContentElements/accordion.svg'
+    ],
+    'content-bootstrappackage-accordion-item' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:bootstrap_package/Resources/Public/Icons/ContentElements/accordion-item.svg'
+    ],
+    'content-bootstrappackage-card-group' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:bootstrap_package/Resources/Public/Icons/ContentElements/card-group.svg'
+    ],
+    'content-bootstrappackage-card-group-item' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:bootstrap_package/Resources/Public/Icons/ContentElements/card-group-item.svg'
+    ],
+    'content-bootstrappackage-carousel' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:bootstrap_package/Resources/Public/Icons/ContentElements/carousel.svg'
+    ],
+    'content-bootstrappackage-carousel-item' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:bootstrap_package/Resources/Public/Icons/ContentElements/carousel-item.svg'
+    ],
+    'content-bootstrappackage-carousel-item-backgroundimage' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:bootstrap_package/Resources/Public/Icons/ContentElements/carousel-item-backgroundimage.svg'
+    ],
+    'content-bootstrappackage-carousel-item-calltoaction' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:bootstrap_package/Resources/Public/Icons/ContentElements/carousel-item-calltoaction.svg'
+    ],
+    'content-bootstrappackage-carousel-item-header' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:bootstrap_package/Resources/Public/Icons/ContentElements/carousel-item-header.svg'
+    ],
+    'content-bootstrappackage-carousel-item-html' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:bootstrap_package/Resources/Public/Icons/ContentElements/carousel-item-html.svg'
+    ],
+    'content-bootstrappackage-carousel-item-image' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:bootstrap_package/Resources/Public/Icons/ContentElements/carousel-item-image.svg'
+    ],
+    'content-bootstrappackage-carousel-item-text' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:bootstrap_package/Resources/Public/Icons/ContentElements/carousel-item-text.svg'
+    ],
+    'content-bootstrappackage-carousel-item-textandimage' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:bootstrap_package/Resources/Public/Icons/ContentElements/carousel-item-textandimage.svg'
+    ],
+    'content-bootstrappackage-beside-text-img-centered-left' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:bootstrap_package/Resources/Public/Icons/ContentElements/beside-text-img-centered-left.svg'
+    ],
+    'content-bootstrappackage-beside-text-img-centered-right' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:bootstrap_package/Resources/Public/Icons/ContentElements/beside-text-img-centered-right.svg'
+    ],
+    'content-bootstrappackage-csv' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:bootstrap_package/Resources/Public/Icons/ContentElements/csv.svg'
+    ],
+    'content-bootstrappackage-externalmedia' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:bootstrap_package/Resources/Public/Icons/ContentElements/externalmedia.svg'
+    ],
+    'content-bootstrappackage-gallery' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:bootstrap_package/Resources/Public/Icons/ContentElements/gallery.svg'
+    ],
+    'content-bootstrappackage-icon-group' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:bootstrap_package/Resources/Public/Icons/ContentElements/icon-group.svg'
+    ],
+    'content-bootstrappackage-icon-group-item' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:bootstrap_package/Resources/Public/Icons/ContentElements/icon-group-item.svg'
+    ],
+    'content-bootstrappackage-listgroup' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:bootstrap_package/Resources/Public/Icons/ContentElements/listgroup.svg'
+    ],
+    'content-bootstrappackage-menu-card' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:bootstrap_package/Resources/Public/Icons/ContentElements/menu-card.svg'
+    ],
+    'content-bootstrappackage-social-links' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:bootstrap_package/Resources/Public/Icons/ContentElements/social-links.svg'
+    ],
+    'content-bootstrappackage-tab' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:bootstrap_package/Resources/Public/Icons/ContentElements/tab.svg'
+    ],
+    'content-bootstrappackage-tab-item' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:bootstrap_package/Resources/Public/Icons/ContentElements/tab-item.svg'
+    ],
+    'content-bootstrappackage-texticon' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:bootstrap_package/Resources/Public/Icons/ContentElements/texticon.svg'
+    ],
+    'content-bootstrappackage-timeline' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:bootstrap_package/Resources/Public/Icons/ContentElements/timeline.svg'
+    ],
+    'content-bootstrappackage-timeline-item' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:bootstrap_package/Resources/Public/Icons/ContentElements/timeline-item.svg'
+    ],
+];
