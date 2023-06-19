2775146d21660b1c09bbd9ab093672bfdad326bc
Benjamin Kott <benjamin.kott@outlook.com>
2023-06-20 01:00:56 +0200

    [TASK] Make main navigation more accessible (#1385)

--- Resources/Public/JavaScript/Src/bootstrap.stickyheader.js
+++ Resources/Public/JavaScript/Src/bootstrap.stickyheader.js
@@ -3,7 +3,7 @@ window.addEventListener('DOMContentLoaded', function () {
     var stickyheader = document.querySelectorAll(".navbar-fixed-top");
     if (stickyheader.length >= 1) {
         function animateHeader() {
-            150 < window.scrollY ? stickyheader[0].classList.add("navbar-transition") : stickyheader[0].classList.remove("navbar-transition");
+            120 < window.scrollY ? stickyheader[0].classList.add("navbar-transition") : stickyheader[0].classList.remove("navbar-transition");
         }
         ['scroll', 'resize', 'DOMContentLoaded'].forEach(function (e) {
             window.addEventListener(e, animateHeader);
