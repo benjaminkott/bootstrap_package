e1578931f7ec545b21b3a58a43914a50d921834c
Benjamin Kott <benjamin.kott@outlook.com>
2023-08-28 14:54:40 +0200

    [TASK] Migrate grid column view element to esm

---
+++ Configuration/JavaScriptModules.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * This file is part of the package bk2k/bootstrap-package.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+return [
+    'dependencies' => [
+        'core',
+        'form',
+    ],
+    'imports' => [
+        '@bk2k/bootstrap-package/' => 'EXT:bootstrap_package/Resources/Public/JavaScript/ESM/',
+    ],
+];
