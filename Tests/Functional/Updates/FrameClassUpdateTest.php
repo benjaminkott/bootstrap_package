9125f1f2039acfdf165a390a081f248cb10f65a5
Benjamin Kott <benjamin.kott@outlook.com>
2023-10-17 10:22:24 +0200

    [BUGFIX] Add missing core extension requirements to functionals (#1435)

--- Tests/Functional/Updates/FrameClassUpdateTest.php
+++ Tests/Functional/Updates/FrameClassUpdateTest.php
@@ -21,6 +21,11 @@ use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
  */
 final class FrameClassUpdateTest extends FunctionalTestCase
 {
+    protected array $coreExtensionsToLoad = [
+        'seo',
+        'rte_ckeditor',
+    ];
+
     protected array $testExtensionsToLoad = [
         'typo3conf/ext/bootstrap_package'
     ];
