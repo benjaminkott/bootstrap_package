a04a042c34ad344df5df06cb4dadfcb4546bbf09
Benjamin Kott <benjamin.kott@outlook.com>
2024-10-16 14:05:04 +0200

    [BUGFIX] Correct codeEditor check for v13

--- Configuration/TCA/Overrides/400_bootstrappackage_carousel_item.php
+++ Configuration/TCA/Overrides/400_bootstrappackage_carousel_item.php
@@ -7,14 +7,30 @@
  * LICENSE file that was distributed with this source code.
  */
 
+use TYPO3\CMS\Core\Information\Typo3Version;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
 defined('TYPO3') or die('Access denied.');
 
-// Activate T3EDITOR if extension is activated
-if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('t3editor')) {
+// v12
+if (ExtensionManagementUtility::isLoaded('t3editor')) {
     $GLOBALS['TCA']['tx_bootstrappackage_carousel_item']['types']['html']['columnsOverrides'] = [
         'bodytext' => [
             'config' => [
                 'renderType' => 't3editor',
+                'wrap' => 'off',
+                'format' => 'html',
+            ],
+        ],
+    ];
+}
+
+// v13 onwards
+if ((new Typo3Version)->getMajorVersion() >= 13) {
+    $GLOBALS['TCA']['tx_bootstrappackage_carousel_item']['types']['html']['columnsOverrides'] = [
+        'bodytext' => [
+            'config' => [
+                'renderType' => 'codeEditor',
                 'wrap' => 'off',
                 'format' => 'html',
             ],
