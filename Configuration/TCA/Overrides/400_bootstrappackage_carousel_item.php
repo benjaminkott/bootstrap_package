8693864fd89d3e0d28dbf863818514dc982d38f3
Benjamin Kott <benjamin.kott@outlook.com>
2024-11-04 06:40:01 +0100

    [TASK] Drop v12 fallback for codeEditor

--- Configuration/TCA/Overrides/400_bootstrappackage_carousel_item.php
+++ Configuration/TCA/Overrides/400_bootstrappackage_carousel_item.php
@@ -12,28 +12,12 @@ use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 defined('TYPO3') or die('Access denied.');
 
-// v12
-if (ExtensionManagementUtility::isLoaded('t3editor')) {
-    $GLOBALS['TCA']['tx_bootstrappackage_carousel_item']['types']['html']['columnsOverrides'] = [
-        'bodytext' => [
-            'config' => [
-                'renderType' => 't3editor',
-                'wrap' => 'off',
-                'format' => 'html',
-            ],
+$GLOBALS['TCA']['tx_bootstrappackage_carousel_item']['types']['html']['columnsOverrides'] = [
+    'bodytext' => [
+        'config' => [
+            'renderType' => 'codeEditor',
+            'wrap' => 'off',
+            'format' => 'html',
         ],
-    ];
-}
-
-// v13 onwards
-if ((new Typo3Version)->getMajorVersion() >= 13) {
-    $GLOBALS['TCA']['tx_bootstrappackage_carousel_item']['types']['html']['columnsOverrides'] = [
-        'bodytext' => [
-            'config' => [
-                'renderType' => 'codeEditor',
-                'wrap' => 'off',
-                'format' => 'html',
-            ],
-        ],
-    ];
-}
+    ],
+];
