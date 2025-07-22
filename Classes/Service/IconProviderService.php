67324d64c18a546186a9f28371ac31cec9377e71
Marvin Buchmann <marvin.buchmann@typo3.com>
2025-07-22 14:48:24 +0200

    [BUGFIX] Check icon providers for duplicates to sustain compatibility with hooks

--- Classes/Service/IconProviderService.php
+++ Classes/Service/IconProviderService.php
@@ -24,10 +24,22 @@ final readonly class IconProviderService
 
     public function __invoke(ModifyIconProvidersEvent $event): void
     {
+        $classList = [];
         $iconProviders = $event->getIconProviders();
+
+        // add already registered class names to prevent duplicate registration
+        array_walk($iconProviders, static function (IconProviderInterface $iconProvider) use (&$classList) {
+            $classList[] = get_class($iconProvider);
+        });
+
         /** @var IconProviderInterface $iconProvider */
         foreach ($this->iconProviders as $iconProvider) {
+            $className = get_class($iconProvider);
+            if (in_array($className, $classList, true)) {
+                continue;
+            }
             $iconProviders[] = $iconProvider;
+            $classList[] = $className;
         }
 
         $event->setIconProviders($iconProviders);
