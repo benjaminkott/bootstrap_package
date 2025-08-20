63a2b96bb44bc351f4eda6f85c033d0289160dbd
Benjamin Kott <benjamin.kott@outlook.com>
2025-08-20 09:18:38 +0200

    [TASK] Set version to 16.0.0-dev

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,5 +34,5 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Benjamin Kott',
     'author_email' => 'info@bk2k.info',
     'author_company' => 'private',
-    'version' => '16.0.0',
+    'version' => '16.0.0-dev',
 ];
