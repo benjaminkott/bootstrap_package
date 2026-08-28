bac4bd430d84ef14704d747b4517370e4191fb30
Benjamin Kott <benjamin.kott@outlook.com>
2026-08-28 10:21:04 +0200

    [BUGFIX] Restrict the referrer policy to online media
    The media rendering partial serves online media and locally hosted video
    alike. Online media renders as an iframe, a local file as a video tag,
    and referrerpolicy is defined on the former and not on the latter.
    Handed over unconditionally it reached every video tag as an attribute
    HTML does not define there.
    
    The partial now asks whether an online media helper is registered for
    the file and hands the attribute over only then. An empty setting
    renders no attribute at all.
    
    Signed-off-by: Benjamin Kott <benjamin.kott@outlook.com>

---
+++ Classes/ViewHelpers/File/IsOnlineMediaViewHelper.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of the package bk2k/bootstrap-package.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace BK2K\BootstrapPackage\ViewHelpers\File;
+
+use TYPO3\CMS\Core\Resource\File;
+use TYPO3\CMS\Core\Resource\FileReference;
+use TYPO3\CMS\Core\Resource\OnlineMedia\Helpers\OnlineMediaHelperRegistry;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+
+class IsOnlineMediaViewHelper extends AbstractViewHelper
+{
+    /**
+     * @return bool
+     */
+    public function render()
+    {
+        $file = $this->renderChildren();
+
+        return is_object($file)
+            && ($file instanceof FileReference || $file instanceof File)
+            && GeneralUtility::makeInstance(OnlineMediaHelperRegistry::class)->hasOnlineMediaHelper($file->getExtension());
+    }
+}
